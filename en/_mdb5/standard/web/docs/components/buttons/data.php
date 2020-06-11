ecd3738317a0fa76cc90945a961c47682c8acd19
ascensus <szymanskimichal9@gmail.com>
2020-06-11 14:13:10 +0200

    Add mdb5 components directory

---
+++ en/_mdb5/standard/web/docs/components/buttons/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Buttons',
+  'seo_title' => 'Buttons - Bootstrap 5 & Material Design layout',
+  'seo_description' => 'Buttons built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/layout/buttons/',
+  'image' => '',
+  'video' => '',
+);
