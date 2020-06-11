b58e13577deb03de68eaf5c297f7a5cf97f6ae08
ascensus <szymanskimichal9@gmail.com>
2020-06-11 09:41:03 +0200

    Update meta data

--- en/_mdb5/standard/web/docs/layout/_blank/data.php
+++ en/_mdb5/standard/web/docs/layout/_blank/data.php
@@ -1,9 +1,9 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '(Name)',
-  'seo_title' => '(Name) - Bootstrap 5 & Material Design layout',
-  'description' => '(Name) built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'main_title' => '[Name]',
+  'seo_title' => '[Name] - Bootstrap 5 & Material Design [category]',
+  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/',
   'image' => '',
   'video' => '',
