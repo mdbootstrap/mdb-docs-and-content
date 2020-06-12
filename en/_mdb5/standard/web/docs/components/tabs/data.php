9d18d49797481d0b88b8404a7a3f1b5ab04e82ad
ascensus <szymanskimichal9@gmail.com>
2020-06-12 16:01:41 +0200

    Add mdb 5 tabs docs

---
+++ en/_mdb5/standard/web/docs/components/tabs/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Tabs',
+  'seo_title' => 'Tabs - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Tabs built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/tabs/',
+  'image' => '',
+  'video' => '',
+);
