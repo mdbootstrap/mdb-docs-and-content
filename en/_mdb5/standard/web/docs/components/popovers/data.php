527d86412eb832244d4ad0b7cf58f140db3fa086
ascensus <szymanskimichal9@gmail.com>
2020-06-15 16:26:39 +0200

    Add mdb5 popovers docs

---
+++ en/_mdb5/standard/web/docs/components/popovers/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Popovers',
+  'seo_title' => 'Popovers - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Popovers built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/popovers/',
+  'image' => '',
+  'video' => '',
+);
