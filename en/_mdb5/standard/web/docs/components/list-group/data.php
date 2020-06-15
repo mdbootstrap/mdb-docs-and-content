24b241a178a4e81f56a9c1572cbada884bab577e
ascensus <szymanskimichal9@gmail.com>
2020-06-15 12:27:02 +0200

    Add mdb5 list group docs

---
+++ en/_mdb5/standard/web/docs/components/list-group/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'List group',
+  'seo_title' => 'List group - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'List group built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/list-group/',
+  'image' => '',
+  'video' => '',
+);
