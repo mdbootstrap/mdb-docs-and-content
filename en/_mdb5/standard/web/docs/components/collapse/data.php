43aa845790fe7b0235ceefc0a39934ccaf41383f
ascensus <szymanskimichal9@gmail.com>
2020-06-15 07:44:59 +0200

    Add mdb5 carousel docs

--- en/_mdb5/standard/web/docs/components/collapse/data.php
+++ en/_mdb5/standard/web/docs/components/collapse/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Collapse',
   'seo_title' => 'Collapse - Bootstrap 5 & Material Design 2.0 components',
   'description' => 'Collapse built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/collapse/',
   'image' => '',
   'video' => '',
 );
