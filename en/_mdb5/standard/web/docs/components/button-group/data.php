11bb2101fa70a6ba8ce0cd0bba52ed8155e1da72
ascensus <szymanskimichal9@gmail.com>
2020-06-15 09:01:55 +0200

    Add mdb5 button group docs

---
+++ en/_mdb5/standard/web/docs/components/button-group/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Button group',
+  'seo_title' => 'Button group - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Button group built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/button-group/',
+  'image' => '',
+  'video' => '',
+);
