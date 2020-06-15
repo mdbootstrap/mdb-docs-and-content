28cb371e2c012555948f8ba0694b65c66c567083
ascensus <szymanskimichal9@gmail.com>
2020-06-15 17:18:57 +0200

    Add mdb5 spinners docs

---
+++ en/_mdb5/standard/web/docs/components/spinners/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Spinners',
+  'seo_title' => 'Spinners - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Spinners built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/spinners/',
+  'image' => '',
+  'video' => '',
+);
