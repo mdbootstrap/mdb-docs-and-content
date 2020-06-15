a55b66b1893291d1b16da4edebc73d25fb163829
ascensus <szymanskimichal9@gmail.com>
2020-06-15 17:07:49 +0200

    Add mdb5 progress docs

---
+++ en/_mdb5/standard/web/docs/components/progress/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Progress',
+  'seo_title' => 'Progress - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Progress built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/progress/',
+  'image' => '',
+  'video' => '',
+);
