6910c38ce7a6c1c99b1e541bbfd1ac82940857fb
ascensus <szymanskimichal9@gmail.com>
2020-06-15 07:55:49 +0200

    Add mdb5 modal docs

---
+++ en/_mdb5/standard/web/docs/components/modal/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Modal',
+  'seo_title' => 'Modal - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Modal built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/modal/',
+  'image' => '',
+  'video' => '',
+);
