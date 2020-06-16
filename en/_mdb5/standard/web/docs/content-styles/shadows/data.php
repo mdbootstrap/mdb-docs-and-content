8ead521bf3c247bdaffbc9d41531f16e553071d2
ascensus <szymanskimichal9@gmail.com>
2020-06-16 08:45:48 +0200

    Add mdb5 shadows docs

---
+++ en/_mdb5/standard/web/docs/content-styles/shadows/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Shadows',
+  'seo_title' => 'Shadows - Bootstrap 5 & Material Design 2.0 styles',
+  'description' => 'Shadows built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/shadows/',
+  'image' => '',
+  'video' => '',
+);
