c5c28950257ddf961417a648f9732f645694c8bb
ascensus <szymanskimichal9@gmail.com>
2020-06-16 09:26:14 +0200

    Add mdb5 navbar docs

--- en/_mdb5/standard/web/docs/navigation/pills/data.php
+++ en/_mdb5/standard/web/docs/navigation/pills/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Pills',
-  'seo_title' => 'Pills - Bootstrap 5 & Material Design 2.0 navigation',
+  'seo_title' => 'Pills - Bootstrap 5 & Material Design 2.0 component',
   'description' => 'Pills built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/pills/',
   'image' => '',
--- en/_mdb5/standard/web/docs/navigation/tabs/data.php
+++ en/_mdb5/standard/web/docs/navigation/tabs/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Tabs',
-  'seo_title' => 'Tabs - Bootstrap 5 & Material Design 2.0 navigation',
+  'seo_title' => 'Tabs - Bootstrap 5 & Material Design 2.0 component',
   'description' => 'Tabs built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/tabs/',
   'image' => '',
