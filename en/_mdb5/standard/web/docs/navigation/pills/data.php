58eead7041c578cdc3d2fe6b54d2ac607aa881ba
ascensus <szymanskimichal9@gmail.com>
2020-06-16 08:52:13 +0200

    Moe pills and tabs mdb5 to navigation caregory

--- en/_mdb5/standard/web/docs/navigation/pills/data.php
+++ en/_mdb5/standard/web/docs/navigation/pills/data.php
@@ -2,9 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Pills',
-  'seo_title' => 'Pills - Bootstrap 5 & Material Design 2.0 components',
+  'seo_title' => 'Pills - Bootstrap 5 & Material Design 2.0 navigation',
   'description' => 'Pills built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/components/pills/',
+  'url' => 'https://mdbootstrap.com/docs/standard/navigation/pills/',
   'image' => '',
   'video' => '',
 );
