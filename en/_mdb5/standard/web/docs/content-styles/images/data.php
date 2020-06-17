270c1abb9485f080db6fc0178f12bf9de0a64ad9
ascensus <szymanskimichal9@gmail.com>
2020-06-17 08:40:43 +0200

    Add mdb5 images docs

---
+++ en/_mdb5/standard/web/docs/content-styles/images/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Images',
+  'seo_title' => 'Images - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Images built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'image' => '',
+  'video' => '',
+);
