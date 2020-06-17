9f2d38ac701aaf919788b5fa87a80f518cab2609
ascensus <szymanskimichal9@gmail.com>
2020-06-17 10:37:01 +0200

    Add mdb5 animations docs

---
+++ en/_mdb5/standard/web/docs/content-styles/animations/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Animations',
+  'seo_title' => 'Animations - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Animations built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/animations/',
+  'image' => '',
+  'video' => '',
+);
