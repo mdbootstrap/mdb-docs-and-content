b22c183abd54d15e9e9f496a6d1d21d6ffc8d75a
ascensus <szymanskimichal9@gmail.com>
2020-06-17 08:32:42 +0200

    Add mdb5 typography docs

---
+++ en/_mdb5/standard/web/docs/content-styles/typography/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Typography',
+  'seo_title' => 'Typography - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Typography built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/typography/',
+  'image' => '',
+  'video' => '',
+);
