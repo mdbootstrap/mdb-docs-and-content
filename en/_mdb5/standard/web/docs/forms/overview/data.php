d20fa2243634ff4196b218e087cecad95d0d920b
ascensus <szymanskimichal9@gmail.com>
2020-06-22 11:53:09 +0200

    Remove category from mdb5 forms overview

--- en/_mdb5/standard/web/docs/forms/overview/data.php
+++ en/_mdb5/standard/web/docs/forms/overview/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Forms',
-  'seo_title' => 'Forms - Bootstrap 5 & Material Design 2.0 [category]',
+  'seo_title' => 'Forms - Bootstrap 5 & Material Design 2.0',
   'description' => 'Forms built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/overview/',
   'image' => '',
