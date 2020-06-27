5b2698bbeb5aae9b47d668b15a152f856982b9af
ascensus <szymanskimichal9@gmail.com>
2020-06-27 09:51:21 +0200

    Updat mdb5 layout docs

--- en/_mdb5/standard/web/docs/layout/grid/data.php
+++ en/_mdb5/standard/web/docs/layout/grid/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Grid system - overview',
+  'main_title' => 'Grid system',
   'seo_title' => 'Bootstrap 5 grid system - examples and tutorial',
   'description' => 'Bootstrap grid is a powerful system for building mobile-first layouts. It is very extensive tool with a great number of options. Below we present you the core knowledge. It is a simplified overview of the most commonly used examples.',
-  'url' => 'https://mdbootstrap.com/docs/standard/layout/overview/',
+  'url' => 'https://mdbootstrap.com/docs/standard/layout/grid/',
   'image' => '',
   'video' => '',
 );
