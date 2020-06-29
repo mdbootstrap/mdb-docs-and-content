9d23bc21697f9ff1ab5cee6117541f83c728f1f5
ascensus <szymanskimichal9@gmail.com>
2020-06-29 16:43:41 +0200

    Add mdb5 hrozintal align docs

--- en/_mdb5/standard/web/docs/layout/horizontal-alignment/data.php
+++ en/_mdb5/standard/web/docs/layout/horizontal-alignment/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Horizontal alignment',
+  'seo_title' => 'Horizontal alignment - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Horizontal alignment for with the latest Bootstrap 5 & Material Design 2.0. Multiple examples & a user-friendly guide',
+  'url' => 'https://mdbootstrap.com/docs/standard/layout/horizontal-alignment/',
   'image' => '',
   'video' => '',
 );
