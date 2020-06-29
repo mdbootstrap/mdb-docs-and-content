b16ea5147597f1f7d9a15b7e6be05b1f45cf70c0
ascensus <szymanskimichal9@gmail.com>
2020-06-29 13:30:10 +0200

    Add mdb5 flexbox docs

---
+++ en/_mdb5/standard/web/docs/layout/flexbox/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Flexbox',
+  'seo_title' => 'Flexbox - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Flexbox utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/layout/flexbox/',
+  'image' => '',
+  'video' => '',
+);
