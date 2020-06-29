c0bbc531d16caddfb909fce5ef337e8f808d6f6a
ascensus <szymanskimichal9@gmail.com>
2020-06-29 11:36:35 +0200

    Add mdb5 colors docs

---
+++ en/_mdb5/standard/web/docs/content-styles/colors/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Colors',
+  'seo_title' => 'Colors - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Material Design color palette for Bootstrap 5. Multiple examples, a user-friendly guide and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/colors/',
+  'image' => '',
+  'video' => '',
+);
