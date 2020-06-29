ba9aa954ed7276b2fce856cfc53491e77cf6d116
ascensus <szymanskimichal9@gmail.com>
2020-06-29 16:15:04 +0200

    Add mdb5 vertical align docs

---
+++ en/_mdb5/standard/web/docs/layout/vertical-alignment/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vertical alignment',
+  'seo_title' => 'Vertical alignment - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Vertical alignment for with the latest Bootstrap 5 & Material Design 2.0. Multiple examples & a user-friendly guide',
+  'url' => 'https://mdbootstrap.com/docs/standard/layout/vertical-alignment/',
+  'image' => '',
+  'video' => '',
+);
