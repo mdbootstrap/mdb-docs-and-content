4379f171d20e523e5812798cf5a9ee5bb0a1546b
ascensus <szymanskimichal9@gmail.com>
2020-07-07 09:50:13 +0200

    Fix masks data

--- en/_mdb5/standard/web/docs/content-styles/masks/data.php
+++ en/_mdb5/standard/web/docs/content-styles/masks/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Masks',
-  'seo_title' => 'Masks - Bootstrap 5 & Material Design 2.0 [category]',
+  'seo_title' => 'Masks - Bootstrap 5 & Material Design 2.0',
   'description' => 'Masks built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/content-styles/masks/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/masks/masks.jpg',
