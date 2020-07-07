d545469748f563d66061c85e4b0a543d6f5708b0
ascensus <szymanskimichal9@gmail.com>
2020-07-07 10:33:08 +0200

    Add ripple section to the masks docs

--- en/_mdb5/standard/web/docs/content-styles/covers/data.php
+++ en/_mdb5/standard/web/docs/content-styles/covers/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
-  'image' => '',
-  'video' => '',
+  'main_title' => 'Covers',
+  'seo_title' => 'Covers - Bootstrap 5 & Material Design 2.0 [category]',
+  'description' => 'Covers built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/covers/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/covers/covers.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
