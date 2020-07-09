0628ca5f90877e0d292524f3a2806c683bdc8edf
ascensus <szymanskimichal9@gmail.com>
2020-07-09 14:38:32 +0200

    Add mdb5 utilities docs

---
+++ en/_mdb5/standard/web/docs/utilities/overflow/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => '[Name]',
+  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0',
+  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/utilities/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
