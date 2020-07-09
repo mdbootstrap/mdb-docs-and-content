265f7c69c4939356298accd0a7277dcb6b4de9de
ascensus <szymanskimichal9@gmail.com>
2020-07-09 15:40:00 +0200

    Add streeched link docs

--- en/_mdb5/standard/web/docs/utilities/screen-readers/data.php
+++ en/_mdb5/standard/web/docs/utilities/screen-readers/data.php
@@ -5,6 +5,6 @@ $data_var = array(
   'seo_title' => 'Screen readers - Bootstrap 5 & Material Design 2.0',
   'description' => 'Screen readers utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/utilities/screen-readers/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/Screen readers/screen-readers.jpg',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/screen-readers/screen-readers.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
