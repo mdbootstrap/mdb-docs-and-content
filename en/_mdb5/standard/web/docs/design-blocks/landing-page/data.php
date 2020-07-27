638de014204675407f776b56dfb3881d3905eb43
ascensus <szymanskimichal9@gmail.com>
2020-07-27 09:34:42 +0200

    Add design blocks docs

---
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Landing page blocks',
+  'seo_title' => 'Landing page blocks - Bootstrap 5 & Material Design 2.0 design blocks',
+  'description' => 'Landing page blocks built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/standard/web/docs/design-blocks/modals/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Modals',
+  'seo_title' => 'Modals - Bootstrap 5 & Material Design 2.0 design blocks',
+  'description' => 'Modals built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/modals/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
