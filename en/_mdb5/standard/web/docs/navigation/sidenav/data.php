5544a28384380af709fd5c552384025ee2c5b44a
ascensus <szymanskimichal9@gmail.com>
2020-08-10 08:01:54 +0200

    Add sidenav docs

--- en/_mdb5/standard/web/docs/navigation/sidenav/data.php
+++ en/_mdb5/standard/web/docs/navigation/sidenav/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Sidenav',
+  'seo_title' => 'Sidenav - Bootstrap 5 & Material Design 2.0 navigation',
+  'description' => 'Sidenav built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/navigation/sidenav/',
   'image' => '',
   'video' => '',
 );
