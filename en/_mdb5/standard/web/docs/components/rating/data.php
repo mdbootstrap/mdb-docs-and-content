85b5a55b53278419e6676c838563c6ed867fc1a5
ascensus <szymanskimichal9@gmail.com>
2020-08-10 09:15:02 +0200

    Add mdb5 rating docs

--- en/_mdb5/standard/web/docs/components/rating/data.php
+++ en/_mdb5/standard/web/docs/components/rating/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 components',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Rating',
+  'seo_title' => 'Rating - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Rating built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/rating/',
   'image' => '',
   'video' => '',
 );
