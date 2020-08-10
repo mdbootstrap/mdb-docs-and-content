93c826494c67ac0c72726736e315521eb570c5b0
ascensus <szymanskimichal9@gmail.com>
2020-08-10 08:29:34 +0200

    Add mdb5 lightbox docs

--- en/_mdb5/standard/web/docs/components/lightbox/data.php
+++ en/_mdb5/standard/web/docs/components/lightbox/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 components',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Lightbox',
+  'seo_title' => 'Lightbox - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Lightbox built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/lightbox/',
   'image' => '',
   'video' => '',
 );
