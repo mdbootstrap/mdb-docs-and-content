3146a898c1f605c63115f8aebfea055edeb0e6e4
ascensus <szymanskimichal9@gmail.com>
2020-08-10 09:07:37 +0200

    Update timepicker data

--- en/_mdb5/standard/web/docs/forms/timepicker/data.php
+++ en/_mdb5/standard/web/docs/forms/timepicker/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Timepicker',
+  'seo_title' => 'Timepicker - Bootstrap 5 & Material Design 2.0 forms',
+  'description' => 'Timepicker built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/timepicker/',
   'image' => '',
   'video' => '',
 );
