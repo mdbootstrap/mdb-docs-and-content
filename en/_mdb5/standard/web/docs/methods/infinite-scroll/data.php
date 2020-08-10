18eff6caa3604770f0a48a88687812a7a24d21c5
ascensus <szymanskimichal9@gmail.com>
2020-08-10 09:30:16 +0200

    Add infinite scroll docs

--- en/_mdb5/standard/web/docs/methods/infinite-scroll/data.php
+++ en/_mdb5/standard/web/docs/methods/infinite-scroll/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Infinite scroll',
+  'seo_title' => 'Infinite scroll - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Infinite scroll built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/methods/infinite-scroll/',
   'image' => '',
   'video' => '',
 );
