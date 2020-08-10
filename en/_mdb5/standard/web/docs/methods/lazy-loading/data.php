83b9c6013f4c9ce81546bdd7562cb16cba8696f4
ascensus <szymanskimichal9@gmail.com>
2020-08-10 09:50:18 +0200

    Add lazy loading docs

--- en/_mdb5/standard/web/docs/methods/lazy-loading/data.php
+++ en/_mdb5/standard/web/docs/methods/lazy-loading/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Lazy loading',
+  'seo_title' => 'Lazy loading - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Lazy loading built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/methods/lazy-loading/',
   'image' => '',
   'video' => '',
 );
