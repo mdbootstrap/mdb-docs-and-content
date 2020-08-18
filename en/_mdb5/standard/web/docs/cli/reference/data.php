c2aff8ae150014fcada35f4306453e997fbbf009
ascensus <szymanskimichal9@gmail.com>
2020-08-18 12:06:11 +0200

    Update components descirption margins

--- en/_mdb5/standard/web/docs/cli/reference/data.php
+++ en/_mdb5/standard/web/docs/cli/reference/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'CLI Command Reference',
-  'seo_title' => 'CLI Command Reference',
+  'seo_title' => 'MDB CLI Command Reference',
   'description' => 'Create, deploy and host your MDB project with a single command',
   'url' => 'https://mdbootstrap.com/docs/standard/cli/about/',
   'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
