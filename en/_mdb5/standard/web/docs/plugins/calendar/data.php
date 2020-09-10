edd89d08efb0bb3c514bc6ace1b6c3e56aef37b5
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-09-10 11:25:49 +0200

    MDB5 Calendar docs update

--- en/_mdb5/standard/web/docs/plugins/calendar/data.php
+++ en/_mdb5/standard/web/docs/plugins/calendar/data.php
@@ -1,10 +1,13 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'main_title' => 'Calendar',
+  'seo_title' => 'Calendar - Bootstrap 5 & Material Design 2.0 [category]',
+  'description' => 'Calendar is a plugin that allows you to efficiently manage tasks. Thanks to
+  this extension you will be able to easily create new events, manage current
+  events, move existing events between other days, and store all data in an easily
+  readable array',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/calendar/',
   'image' => '',
   'video' => '',
 );
