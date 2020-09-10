8676d91b9ae8c6169a5d5fd90d0c137a6d790651
ascensus <szymanskimichal9@gmail.com>
2020-09-10 11:05:18 +0200

    Add plugins directory

---
+++ en/_mdb5/standard/web/docs/plugins/_blank/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => '[Name]',
+  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 [category]',
+  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'image' => '',
+  'video' => '',
+);
