3e7a78670b68483db0d8d69e61676214599bd3da
ascensus <szymanskimichal9@gmail.com>
2020-09-10 15:19:25 +0200

    Update plugins files

---
+++ en/_mdb5/standard/web/docs/methods/loading-management/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Loading management',
+  'seo_title' => 'Loading management - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Loading management built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/methods/loading-management/',
+  'image' => '',
+  'video' => '',
+);
