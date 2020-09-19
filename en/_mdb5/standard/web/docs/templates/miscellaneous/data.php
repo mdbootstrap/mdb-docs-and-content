a5b9f7147df3844c40b01a5f7b1a00f9fd206123
ascensus <szymanskimichal9@gmail.com>
2020-09-19 14:11:03 +0200

    Add miscellaneous templates docs

--- en/_mdb5/standard/web/docs/templates/miscellaneous/data.php
+++ en/_mdb5/standard/web/docs/templates/miscellaneous/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 design blocks',
-  'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => '',
+  'main_title' => 'Miscellaneous templates',
+  'seo_title' => 'Miscellaneous templates - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Miscellaneous templates built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/templates/miscellaneous/',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
