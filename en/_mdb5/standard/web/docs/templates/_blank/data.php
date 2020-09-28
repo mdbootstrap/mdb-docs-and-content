fd4e7ddc800235ee0e8120861da46197114a4efc
ascensus <szymanskimichal9@gmail.com>
2020-09-28 10:57:56 +0200

    Add blog template docs

--- en/_mdb5/standard/web/docs/templates/_blank/data.php
+++ en/_mdb5/standard/web/docs/templates/_blank/data.php
@@ -2,9 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => '[Name]',
-  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 design blocks',
+  'seo_title' => '[Name] - Bootstrap 5 & Material Design 2.0 templates',
   'description' => '[Name] built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => '',
+  'url' => 'https://mdbootstrap.com/docs/standard/templates/',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
