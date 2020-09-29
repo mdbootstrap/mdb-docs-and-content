6ba1323e9482cfc7059c4906b8cd58eb8c0d17df
filipkappa <f.kapusta@mdbootstrap.com>
2020-09-29 17:41:14 +0200

    add buttons translated in browser

---
+++ cn/_mdb5/standard/web/docs/components/buttons/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Buttons',
+  'seo_title' => 'Buttons - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Buttons built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/buttons/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/buttons/buttons.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
