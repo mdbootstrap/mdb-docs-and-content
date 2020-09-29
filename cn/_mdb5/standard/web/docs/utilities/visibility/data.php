9d36ade8fdc46ba1811b94cb1ea525a74a2a6c57
Dawid <d.adach@mdbootstrap.com>
2020-09-29 14:30:03 +0200

    Add data.php and remianing not translated files

---
+++ cn/_mdb5/standard/web/docs/utilities/visibility/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Visibility',
+  'seo_title' => 'Visibility - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Visibility utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive api, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/utilities/visibility/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/visibility/visibility.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
