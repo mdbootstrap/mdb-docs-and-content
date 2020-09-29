013581d216dab7da9f739774db63959eef61aab3
Filip <f.kapusta@mdbootstrap.com>
2020-09-29 14:46:32 +0200

    add cn alerts

---
+++ cn/_mdb5/standard/web/docs/components/alerts/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Alerts',
+  'seo_title' => 'Alerts - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Alerts built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/alerts/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/alerts/alerts.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
