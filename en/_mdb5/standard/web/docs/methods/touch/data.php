a8dcccf9b4a8ba3a5e5cf39c56c509fcfa8394cf
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-10-02 09:21:06 +0200

    MDB5 Touch page update

--- en/_mdb5/standard/web/docs/methods/touch/data.php
+++ en/_mdb5/standard/web/docs/methods/touch/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Touch',
-  'seo_title' => 'Touch - Bootstrap 5 & Material Design 2.0 [category]',
+  'seo_title' => 'Touch - Bootstrap 5 & Material Design 2.0',
   'description' => 'Touch built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/methods/touch/',
   'image' => '',
