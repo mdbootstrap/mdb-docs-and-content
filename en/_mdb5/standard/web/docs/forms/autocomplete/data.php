5539cc57d6b8826a5c0488c6feb074081db42c64
idzark <a.idzikowski91@gmail.com>
2020-10-12 09:42:04 +0200

    Add mdb5 autocomplete docs

---
+++ en/_mdb5/standard/web/docs/forms/autocomplete/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Autocomplete',
+  'seo_title' => 'Autocomplete - Bootstrap 5 & Material Design 2.0 forms',
+  'description' => 'Autocomplete built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/autocomplete/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
