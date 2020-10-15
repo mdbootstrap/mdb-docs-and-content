d4b0020050f35c51dba65e139a1747eab50d9b82
Marcin Luczak <mbukacz@gmail.com>
2020-10-15 09:25:04 +0200

    add Parallax docs

---
+++ en/_mdb5/standard/web/docs/plugins/parallax/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Parallax',
+  'seo_title' => 'Parallax - Bootstrap 5 & Material Design 2.0 plugin',
+  'description' => 'Parallax built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/parallax/',
+  'image' => '',
+  'video' => '',
+);
