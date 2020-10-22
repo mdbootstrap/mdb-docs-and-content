b06713927b157a56a3b27d9abf61704c877ee589
Marcin Luczak <mbukacz@gmail.com>
2020-10-22 08:56:50 +0200

    Add Input Mask docs

---
+++ en/_mdb5/standard/web/docs/plugins/input-mask/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Input Mask',
+  'seo_title' => 'Input Mask - Bootstrap 5 & Material Design 2.0 plugin',
+  'description' => 'Input Mask built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/input-mask/',
+  'image' => '',
+  'video' => '',
+);
