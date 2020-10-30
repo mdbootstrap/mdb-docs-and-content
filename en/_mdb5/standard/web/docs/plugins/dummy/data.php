ea4b15cc2ce693e717711bc617e186e736e4e4b2
Marcin Luczak <mbukacz@gmail.com>
2020-10-30 11:12:16 +0100

    add Dummy files

---
+++ en/_mdb5/standard/web/docs/plugins/dummy/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Dummy',
+  'seo_title' => 'Dummy - Bootstrap 5 & Material Design 2.0 plugin',
+  'description' => 'Dummy built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/dummy/',
+  'image' => '',
+  'video' => '',
+);
