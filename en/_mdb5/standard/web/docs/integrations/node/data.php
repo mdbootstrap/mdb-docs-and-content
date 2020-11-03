8d29a614ca495a5812bc5f3c7cc407c924ccf09b
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-11-03 12:30:15 +0100

    MDB5 Node integration docs

---
+++ en/_mdb5/standard/web/docs/integrations/node/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Node integration',
+  'seo_title' => 'Node - Bootstrap 5 & Material Design 2.0 integration',
+  'description' => 'Node integration built with Express, Sequelize, MySQL, dotenv, the latest Bootstrap 5 & Material Design 2.0',
+  'url' => 'https://mdbootstrap.com/docs/standard/integrations/node/',
+  'image' => '',
+  'video' => '',
+);
