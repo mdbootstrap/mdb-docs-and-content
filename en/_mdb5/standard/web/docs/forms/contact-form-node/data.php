a0ac115e82c633cfaeb9ae7a20d427c278fd6953
Marcin Luczak <mbukacz@gmail.com>
2020-11-06 15:19:57 +0100

    separate contact form into php and nodejs folders, create nodejs basic page content

---
+++ en/_mdb5/standard/web/docs/forms/contact-form-node/data.php
@@ -0,0 +1,13 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Contact Form - NodeJS',
+  'seo_title' => 'Bootstrap 5 & Material Design 2.0',
+  'description' => 'This is a step-by-step guide tutorial on how to easily create beautiful contact
+  forms using the Bootstrap framework, JavaScript and NodeJS environment with Express server. It also covers validation of data, sending
+  requests using AJAX and other form related topics. If you have any questions, please do not hesitate
+  to post a question on our <a href="https://mdbootstrap.com/support/">support forum</a>.',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/contact-form/',
+  'image' => '',
+  'video' => '',
+);
