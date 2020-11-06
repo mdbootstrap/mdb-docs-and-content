e28c9b60ddbef81847de2107bf524e42a3d6ea44
Piotr <p.glejzer@mdbootstrap.com>
2020-11-06 10:48:52 +0100

    add chips docs

---
+++ en/_mdb5/standard/web/docs/components/chips/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Chips',
+  'seo_title' => 'Chips - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Chips built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/',
+  'image' => '',
+  'video' => '',
+);
