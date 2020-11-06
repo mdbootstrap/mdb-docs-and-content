b96a1131932f0fb0a3bc301cbb8272f99bb967c8
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-11-06 13:08:14 +0100

    MDB5 Node docs update

---
+++ en/_mdb5/standard/web/docs/integrations/admin-dashboards-nodejs-expressjs/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Admin Dashboard + Node.js + Express.js',
+  'seo_title' => 'Bootstrap 5 integration with Node.js & Express.js',
+  'description' => 'MDB Integration - Admin Dashboard built with the latest Bootstrap 5, Node.js, Express.js & Material Design 2.0',
+  'url' => 'https://mdbootstrap.com/docs/standard/integrations/admin-dashboards-nodejs-expressjs/',
+  'image' => '',
+  'video' => '',
+);
