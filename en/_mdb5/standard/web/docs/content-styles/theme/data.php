d7a7c3a78551df16432f84366da16f000c707f41
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-11-09 10:24:06 +0100

    MDB5 theme url update

--- en/_mdb5/standard/web/docs/content-styles/theme/data.php
+++ en/_mdb5/standard/web/docs/content-styles/theme/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Theme',
   'seo_title' => 'Theme - Bootstrap 5 & Material Design 2.0',
   'description' => 'Theme utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/layout/theme/',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/theme/',
   'image' => '',
   'video' => '',
 );
