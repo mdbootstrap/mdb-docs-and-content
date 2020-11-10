76d0a65a8246bde8455c08efe23fdca78741f092
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-11-10 07:57:14 +0100

    MDB5 integrations meta data update

--- en/_mdb5/standard/web/docs/integrations/admin-dashboard-nodejs-expressjs/data.php
+++ en/_mdb5/standard/web/docs/integrations/admin-dashboard-nodejs-expressjs/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Admin Dashboard + Node.js + Express.js',
   'seo_title' => 'Bootstrap 5 integration with Node.js & Express.js',
   'description' => 'MDB Integration - Admin Dashboard built with the latest Bootstrap 5, Node.js, Express.js & Material Design 2.0',
-  'url' => 'https://mdbootstrap.com/docs/standard/integrations/admin-dashboards-nodejs-expressjs/',
+  'url' => 'https://mdbootstrap.com/docs/standard/integrations/admin-dashboard-nodejs-expressjs/',
   'image' => '',
   'video' => '',
 );
--- en/_mdb5/standard/web/docs/integrations/admin-dashboard-php-laravel/data.php
+++ en/_mdb5/standard/web/docs/integrations/admin-dashboard-php-laravel/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Admin Dashboard + PHP + Laravel',
   'seo_title' => 'Bootstrap 5 integration with PHP & Laravel',
   'description' => 'MDB Integration - Admin Dashboard built with the latest Bootstrap 5, PHP, Laravel & Material Design 2.0',
-  'url' => 'https://mdbootstrap.com/docs/standard/integrations/admin-dashboards-php-laravel/',
+  'url' => 'https://mdbootstrap.com/docs/standard/integrations/admin-dashboard-php-laravel/',
   'image' => '',
   'video' => '',
 );
