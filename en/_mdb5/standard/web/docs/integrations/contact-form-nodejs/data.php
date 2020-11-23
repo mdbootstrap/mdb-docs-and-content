a9b2c29a3027f6648a8a489c600bd37efe1847d6
ascensus <szymanskimichal9@gmail.com>
2020-11-23 12:23:25 +0100

    Update contact forms meta data

--- en/_mdb5/standard/web/docs/integrations/contact-form-nodejs/data.php
+++ en/_mdb5/standard/web/docs/integrations/contact-form-nodejs/data.php
@@ -2,11 +2,10 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Contact Form - NodeJS',
-  'seo_title' => 'Bootstrap 5 & Material Design 2.0',
+  'seo_title' => 'Bootstrap 5 Contact Form with Node.js',
   'description' => 'This is a step-by-step guide tutorial on how to easily create beautiful contact
   forms using the Bootstrap framework, JavaScript and NodeJS environment with Express server. It also covers validation of data, sending
-  requests using AJAX and other form related topics. If you have any questions, please do not hesitate
-  to post a question on our <a href="https://mdbootstrap.com/support/">support forum</a>.',
+  requests using AJAX and other form related topics.',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/contact-form-node/',
   'image' => '',
   'video' => '',
--- en/_mdb5/standard/web/docs/integrations/contact-form-php/data.php
+++ en/_mdb5/standard/web/docs/integrations/contact-form-php/data.php
@@ -2,11 +2,10 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Contact Form - PHP',
-  'seo_title' => 'Bootstrap 5 & Material Design 2.0',
+  'seo_title' => 'Bootstrap 5 Contact Form with PHP',
   'description' => 'This is a step-by-step guide tutorial on how to easily create beautiful contact
   forms using the Bootstrap framework, PHP and JavaScript. It also covers validation of data, sending
-  requests using AJAX and other form related topics. If you have any questions, please do not hesitate
-  to post a question on our <a href="https://mdbootstrap.com/support/">support forum</a>.',
+  requests using AJAX and other form related topics. ',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/contact-form-php/',
   'image' => '',
   'video' => '',
