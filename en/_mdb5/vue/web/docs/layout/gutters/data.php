d9dca1f95208a81acd80d1e15c1bba63f221c4c9
Marcin Luczak <mbukacz@gmail.com>
2020-12-04 15:15:08 +0100

    Add MDB5 Vue Layout section pages

---
+++ en/_mdb5/vue/web/docs/layout/gutters/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Gutters',
+  'seo_title' => 'Vue Gutters - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Gutters are the padding between your columns, used to responsively space and align content in the Bootstrap grid system.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/layout/gutters/',
+  'image' => '',
+  'video' => '',
+);
