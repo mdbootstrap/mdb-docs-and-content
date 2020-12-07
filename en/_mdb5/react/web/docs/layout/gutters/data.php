f463a35f3b75110d42dadb4ec8f773397cd37fdf
Piotr <glejzerpiotr@gmail.com>
2020-12-07 17:24:34 +0100

    add layouts pages

---
+++ en/_mdb5/react/web/docs/layout/gutters/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Gutters',
+  'seo_title' => 'React Gutters - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Gutters are the padding between your columns, used to responsively space and align content in the Bootstrap grid system.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/layout/gutters/',
+  'image' => '',
+  'video' => '',
+);
