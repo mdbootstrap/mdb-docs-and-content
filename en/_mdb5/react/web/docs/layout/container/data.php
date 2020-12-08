2ad492630147975251f5c8f176e992c8f736be44
Piotr <glejzerpiotr@gmail.com>
2020-12-08 18:25:53 +0100

    add new pages

---
+++ en/_mdb5/react/web/docs/layout/container/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Containers',
+  'seo_title' => 'React Containers - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Containers utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/layout/containers/',
+  'image' => '',
+  'video' => '',
+);
