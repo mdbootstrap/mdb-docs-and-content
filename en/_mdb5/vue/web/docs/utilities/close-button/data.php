3b41b8b1c332cdc913b1e6d165a41a5022d0b041
Marcin Luczak <mbukacz@gmail.com>
2020-12-10 14:38:01 +0100

    Add Utilities views files

---
+++ en/_mdb5/vue/web/docs/utilities/close-button/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Close button',
+  'seo_title' => 'Vue Close button - Bootstrap 5 & Material Design 2.0',
+  'description' => 'A generic close button for dismissing content like modals, toasts and alerts.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/close-button',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/vue/web/docs/utilities/ratio/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Ratio',
+  'seo_title' => 'Vue Ratio - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Ratio built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/embeds/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/embeds/embeds.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
