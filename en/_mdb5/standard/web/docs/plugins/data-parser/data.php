a0fb454cb7fec7e7e1f5e7f79fd228cf859158c9
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-12-15 10:18:04 +0100

    Fix data-parser mdb prefix typo

--- en/_mdb5/standard/web/docs/plugins/data-parser/data.php
+++ en/_mdb5/standard/web/docs/plugins/data-parser/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Data parser',
   'seo_title' => 'Data parser - Bootstrap 5 & Material Design 2.0 plugin',
   'description' => 'Data parser built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/plugins/data-mdb-parser/',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/data-parser/',
   'image' => '',
   'video' => '',
 );
