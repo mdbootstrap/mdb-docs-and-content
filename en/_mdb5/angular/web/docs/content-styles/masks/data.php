11b8aa429ecfdc254b3d4327ab34e3ea5b4cc1b9
idzark <a.idzikowski91@gmail.com>
2020-12-22 13:53:24 +0100

    Update data.php in angular5 docs

--- en/_mdb5/angular/web/docs/content-styles/masks/data.php
+++ en/_mdb5/angular/web/docs/content-styles/masks/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Masks',
   'seo_title' => 'Angular Masks - Bootstrap 5 & Material Design 2.0',
   'description' => 'Masks built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/masks/',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/content-styles/masks/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/masks/masks.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
--- en/_mdb5/angular/web/docs/content-styles/shadows/data.php
+++ en/_mdb5/angular/web/docs/content-styles/shadows/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Shadows',
   'seo_title' => 'Angular Shadows - Bootstrap 5 & Material Design 2.0 styles',
   'description' => 'Shadows built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/shadows/',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/content-styles/shadows/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/shadows/shadows.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
