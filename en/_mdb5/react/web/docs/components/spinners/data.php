73b0fb3733ebfb5855607b36556c8b8835581ccd
Krzysztof Wilk <xedanion16@gmail.com>
2020-12-23 13:26:39 +0100

    add spinners react mdb5 docs

---
+++ en/_mdb5/react/web/docs/components/spinners/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Spinners',
+  'seo_title' => 'React Spinners - Bootstrap 5 & Material Design 2.0 components',
+  'description' => 'Spinners built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/spinners/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/components/spinners/spinners.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
