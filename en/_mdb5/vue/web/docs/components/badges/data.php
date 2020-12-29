4a49f35ac8560a83c0a60adf2711e4da20863121
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2020-12-29 14:16:29 +0100

    Add Vue buttons & button group

--- en/_mdb5/vue/web/docs/components/badges/data.php
+++ en/_mdb5/vue/web/docs/components/badges/data.php
@@ -1,7 +1,7 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Vue Bootstrap Badges',
+  'main_title' => 'Badges',
   'seo_title' => 'Vue Badges - Bootstrap 5 & material Design 2.0',
   'description' => 'Vue Badges built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/b5/vue/components/badges/',
---
+++ en/_mdb5/vue/web/docs/components/buttons/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Buttons',
+  'seo_title' => 'Vue Buttons - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Vue Buttons built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/components/buttons/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/buttons/buttons.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
