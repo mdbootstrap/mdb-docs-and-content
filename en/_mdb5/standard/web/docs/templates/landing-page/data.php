6dd05747856e7d57534cdac9e944e266c9b3461d
mateuszleciejewski <m.leciejewski@mdbootstrap.com>
2021-01-06 12:51:34 +0100

    Add image to landing page template listing page

--- en/_mdb5/standard/web/docs/templates/landing-page/data.php
+++ en/_mdb5/standard/web/docs/templates/landing-page/data.php
@@ -5,6 +5,6 @@ $data_var = array(
   'seo_title' => 'Landing page templates - Bootstrap 5 & Material Design 2.0',
   'description' => 'Landing page templates built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/templates/landig-page/',
-  'image' => '',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2021/01/landingpages.png',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
