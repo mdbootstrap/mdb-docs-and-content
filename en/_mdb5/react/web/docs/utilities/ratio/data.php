c2eb6ab48feac41be1eab68dd9ffdbf9d87f9942
Krzysztof Wilk <xedanion16@gmail.com>
2021-01-07 13:07:41 +0100

    correct ratio and visually-hidden docs in mdb5 react

--- en/_mdb5/react/web/docs/utilities/ratio/data.php
+++ en/_mdb5/react/web/docs/utilities/ratio/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Ratio',
   'seo_title' => 'React Ratio - Bootstrap 5 & Material Design 2.0',
   'description' => 'Ratio built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
-  'url' => 'https://mdbootstrap.com/docs/b5/react/utilities/embeds/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/embeds/embeds.jpg',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/utilities/ratio/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/ratio/ratio.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
