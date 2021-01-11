648fe63e5be17b5039480058be98d60b7179e152
MAKUSref <tomaszmakowski002731@gmail.com>
2021-01-11 13:54:35 +0100

    docs(search, charts): add search docs, fix charts advanced docs

---
+++ en/_mdb5/standard/web/docs/forms/search/data.php
@@ -0,0 +1,9 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Search',
+  'seo_title' => 'Search - Bootstrap 5 & Material Design 2.0 forms',
+  'description' => 'Search built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/search/',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
