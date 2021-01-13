812c0824ffe947eb4f423c9aa0b6a5c41679a9ac
Krzysztof Wilk <xedanion16@gmail.com>
2021-01-13 14:11:28 +0100

    add MDBCheckbox docs for mdb5 react

---
+++ en/_mdb5/react/web/docs/forms/checkbox/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Checkbox',
+  'seo_title' => 'React Checkbox - Bootstrap 5 & Material Design 2.0 forms',
+  'description' => 'Checkbox built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/forms/checkbox/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/forms/checkbox/checkbox.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
