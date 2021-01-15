97959c2fc44470aeb05dfdffa4fc6812023e256f
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2021-01-15 12:37:06 +0100

    Add Vue checkbox

---
+++ en/_mdb5/vue/web/docs/forms/checkbox/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Checkbox',
+  'seo_title' => 'Vue Checkbox - Bootstrap 5 & Material Design 2.0',
+  'description' => 'Vue Checkbox built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/forms/checkbox/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/checkbox/checkbox.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
