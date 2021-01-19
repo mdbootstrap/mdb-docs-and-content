254c960306f04398ca9aa7f853f54f687a87aaad
ascensus <szymanskimichal9@gmail.com>
2021-01-19 14:55:49 +0100

    Update alert meta data

--- en/_mdb5/standard/web/docs/components/alerts/data.php
+++ en/_mdb5/standard/web/docs/components/alerts/data.php
@@ -3,6 +3,7 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Alerts',
   'subheading' => 'Bootstrap 5 Alerts component',
+  'seo_title' => 'Bootstrap Alerts - examples & tutorial',
   'description' => 'Responsive Alerts built with the latest Bootstrap 5. Multiple code examples: alert box, fade out alert, auto close, alert with icons and more',
   'url' => 'https://mdbootstrap.com/docs/standard/components/alerts/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/alerts/alerts.jpg',
