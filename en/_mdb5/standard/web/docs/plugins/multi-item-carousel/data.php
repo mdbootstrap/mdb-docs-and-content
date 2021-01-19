aa13422dde039a8d3959ba7b558fcf595ca0ead7
filipkappa <f.kapusta@mdbootstrap.com>
2021-01-19 14:12:35 +0100

    add seo changes to plugins category

--- en/_mdb5/standard/web/docs/plugins/multi-item-carousel/data.php
+++ en/_mdb5/standard/web/docs/plugins/multi-item-carousel/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Multi item carousel',
-  'seo_title' => 'Multi item carousel - Bootstrap 5 & Material Design 2.0 plugin',
-  'description' => 'Multi item carousel built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Multi item carousel plugin',
+  'seo_title' => 'Bootstrap Multi item carousel - examples & tutorial',
+  'description' => 'Responsive Multi item carousel built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/plugins/multi-item-carousel/',
   'image' => '',
   'video' => '',
--- en/_mdb5/standard/web/docs/plugins/onboarding/data.php
+++ en/_mdb5/standard/web/docs/plugins/onboarding/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Onboarding',
-  'seo_title' => 'Onboarding - Bootstrap 5 & Material Design 2.0 plugin',
-  'description' => 'Onboarding built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Onboarding plugin',
+  'seo_title' => 'Bootstrap Onboarding - examples & tutorial',
+  'description' => 'Responsive Onboarding built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/plugins/onboarding/',
   'image' => '',
   'video' => '',
