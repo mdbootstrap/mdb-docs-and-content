190d420a3f279a3dd26d642d03a994bb5ad005db
unknown <a.jakubowski@mdbootstrap.com>
2021-01-19 14:16:17 +0100

    Data

--- en/_mdb5/standard/web/docs/data/tables/data.php
+++ en/_mdb5/standard/web/docs/data/tables/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Tables',
-  'seo_title' => 'Tables - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Tables built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Tables',
+  'seo_title' => 'Bootstrap Tables - examples & tutorial',
+  'description' => 'Responsive Tables built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/data/tables/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/data/tables/tables.jpg',
   'video' => '',
