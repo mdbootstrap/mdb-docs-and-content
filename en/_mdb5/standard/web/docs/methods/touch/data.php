3fc13866096f6d4da8441b82285de84dd1ed7f33
mateuszleciejewski <m.leciejewski@mdbootstrap.com>
2021-01-19 14:30:44 +0100

    update methods data

--- en/_mdb5/standard/web/docs/methods/touch/data.php
+++ en/_mdb5/standard/web/docs/methods/touch/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Touch',
-  'seo_title' => 'Touch - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Touch built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Touch',
+  'seo_title' => 'Bootstrap Touch - examples & tutorial',
+  'description' => 'Touch built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
   'url' => 'https://mdbootstrap.com/docs/standard/methods/touch/',
   'image' => '',
   'video' => '',
