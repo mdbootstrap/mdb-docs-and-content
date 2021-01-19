a531ab203dee102e2d871a16704e59b516dc3457
mateuszleciejewski <m.leciejewski@mdbootstrap.com>
2021-01-19 14:19:49 +0100

    update forms data

--- en/_mdb5/standard/web/docs/forms/radio/data.php
+++ en/_mdb5/standard/web/docs/forms/radio/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Radio',
-  'seo_title' => 'Radio - Bootstrap 5 & Material Design 2.0 forms',
-  'description' => 'Radio built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Radio component',
+  'seo_title' => 'Bootstrap Radio - examples & tutorial',
+  'description' => 'Radio built with the latest Bootstrap 5. Multiple code examples: with button, checkbox, toggle, inline & many more',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/radio/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/radio/radio.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/standard/web/docs/forms/select/data.php
+++ en/_mdb5/standard/web/docs/forms/select/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Select',
-  'seo_title' => 'Select - Bootstrap 5 & Material Design 2.0 forms',
-  'description' => 'Select built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Select component',
+  'seo_title' => 'Bootstrap Select - examples & tutorial',
+  'description' => 'Responsive Select built with the latest Bootstrap 5. Multiple code examples: with dropdown, search, placeholder, multiselect, validation & many more',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/select/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/select/select.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
