01e90c183a8263de6c41ad1a29f5d16c1c765f5d
ascensus <szymanskimichal9@gmail.com>
2021-01-19 13:03:53 +0100

    Update navbar

--- en/_mdb5/standard/web/docs/navigation/navbar/data.php
+++ en/_mdb5/standard/web/docs/navigation/navbar/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Navbar',
-  'seo_title' => 'Navbar - Bootstrap 5 & Material Design 2.0 component',
-  'description' => 'Navbar built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Bootstrap 5 Navbar component',
+  'seo_title' => 'Bootstrap Navbar - examples & tutorial',
+  'description' => 'Responsive Navbar built with the latest Bootstrap 5. Multiple code examples: with dropdown, fixed, center, vertical, tabs, colors & many more',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/navbar/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/navbar/navbar.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
