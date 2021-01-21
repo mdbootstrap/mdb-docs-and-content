b78fa4059121eb0a8e8fe9ecaff0baba36fcf1df
Piotr <glejzerpiotr@gmail.com>
2021-01-21 15:50:58 +0100

    add new components

---
+++ en/_mdb5/react/web/docs/components/popovers/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Popovers',
+  'subheading' => 'Bootstrap 5 Popover component',
+  'seo_title' => 'React Bootstrap Popovers - examples & tutorial',
+  'description' => 'Responsive Popovers built with the latest Bootstrap 5. Multiple code examples: with html, dynamic content, width manipulation, close button, on hover & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/popovers/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/popovers/popovers.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
