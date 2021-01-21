16d13701cd269801e38135081b2872e22cbeb415
idzark <a.idzikowski91@gmail.com>
2021-01-21 15:19:41 +0100

    Add MDB5 Angular checkbox, inputs and radio docs

---
+++ en/_mdb5/angular/web/docs/forms/checkbox/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Checkbox',
+  'subheading' => 'Angular Bootstrap 5 Checkbox component',
+  'seo_title' => 'Bootstrap Checkbox - examples & tutorial',
+  'description' => 'Responsive Checkbox built with the latest Bootstrap 5. Multiple code examples: with style, group, button, size, list & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/checkbox/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/checkbox/checkbox.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/angular/web/docs/forms/input-fields/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Input fields',
+  'subheading' => 'Angular Bootstrap 5 Input fields',
+  'seo_title' => 'Bootstrap Input fields - examples & tutorial',
+  'description' => 'Input fields built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/input-fields/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/input-fields/input-fields.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
