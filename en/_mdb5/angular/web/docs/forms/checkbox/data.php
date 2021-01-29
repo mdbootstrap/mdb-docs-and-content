d07a9b703cee0e95030e118f4812b8dc45300007
filipkappa <f.kapusta@mdbootstrap.com>
2021-01-29 10:27:55 +0100

    add angular forms data change

--- en/_mdb5/angular/web/docs/forms/checkbox/data.php
+++ en/_mdb5/angular/web/docs/forms/checkbox/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Checkbox',
-  'seo_title' => 'Angular Checkbox - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Responsive Checkbox built with the latest Bootstrap 5. Multiple code examples: with style, group, button, size, list & many more',
+  'subheading' => 'Angular Bootstrap 5 Checkbox',
+  'seo_title' => 'Angular Checkbox - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Checkbox built with the latest Bootstrap 5. Multiple code examples: checked, unchecked, disabled, checkbox list, checkbox value and more. ',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/checkbox/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/checkbox/checkbox.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/angular/web/docs/forms/input-fields/data.php
+++ en/_mdb5/angular/web/docs/forms/input-fields/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Input fields',
-  'seo_title' => 'Angular Input fields - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Input fields built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Angular Bootstrap 5 Input fields',
+  'seo_title' => 'Angular Input fields - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Input fields built with the latest Bootstrap 5. Multiple code examples: material inputs, textarea, file, date, input box, field controls and more.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/input-fields/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/input-fields/input-fields.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
