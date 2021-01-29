05f9d6b066213d219a4334ce7817c2fe93f46c18
unknown <a.jakubowski@mdbootstrap.com>
2021-01-29 10:35:58 +0100

    react navigation meta update

--- en/_mdb5/react/web/docs/navigation/footer/data.php
+++ en/_mdb5/react/web/docs/navigation/footer/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Footer',
-  'seo_title' => 'React Footer - Bootstrap 5 & Material Design 2.0 component',
-  'description' => 'Footer built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'React Bootstrap 5 Footer component',
+  'seo_title' => 'React Footer - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive React Footer built with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/react/navigation/footer/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/footer/footer.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
