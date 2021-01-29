2b114ac74907d9cad809b09812aaa11d5c48a265
filipkappa <f.kapusta@mdbootstrap.com>
2021-01-29 10:18:42 +0100

    add content-styles data changes

--- en/_mdb5/angular/web/docs/content-styles/icons/data.php
+++ en/_mdb5/angular/web/docs/content-styles/icons/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Icons',
-  'seo_title' => 'Angular Icons - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Icons built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Angular Bootstrap 5 Icons',
+  'seo_title' => 'Angular Icons - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Icons built with the latest Bootstrap 5. Multiple code examples: symbols, icon list, font awesome, favicons and more.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/content-styles/icons/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/icons/icons.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/angular/web/docs/content-styles/masks/data.php
+++ en/_mdb5/angular/web/docs/content-styles/masks/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Masks',
-  'seo_title' => 'Angular Masks - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Masks built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide and customization tools',
+  'subheading' => 'Angular Bootstrap 5 Masks',
+  'seo_title' => 'Angular Masks - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Masks built with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/content-styles/masks/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/masks/masks.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
