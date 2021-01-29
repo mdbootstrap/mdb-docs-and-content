b67f8faaf4d6bd2c864440ca0d272b24c9c6682b
unknown <a.jakubowski@mdbootstrap.com>
2021-01-29 10:07:35 +0100

    React content-styles meta update

--- en/_mdb5/react/web/docs/content-styles/shadows/data.php
+++ en/_mdb5/react/web/docs/content-styles/shadows/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Shadows',
-  'seo_title' => 'React Shadows - Bootstrap 5 & Material Design 2.0 styles',
-  'description' => 'Shadows built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'React Bootstrap 5 Shadows component',
+  'seo_title' => 'React Shadows - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive React Shadows built with the latest Bootstrap 5. Multiple code examples: box shadow, inset shadow & many more.',
   'url' => 'https://mdbootstrap.com/docs/b5/react/content-styles/shadows/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/content-styles/shadows/shadows.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
