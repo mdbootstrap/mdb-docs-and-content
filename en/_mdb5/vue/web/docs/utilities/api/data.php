8d38ccc3a3783d939f1b5e099881ee84e8c8845c
mateuszleciejewski <m.leciejewski@mdbootstrap.com>
2021-01-29 12:05:42 +0100

    vue utilities data update 2/2

--- en/_mdb5/vue/web/docs/utilities/api/data.php
+++ en/_mdb5/vue/web/docs/utilities/api/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'API',
-  'seo_title' => 'Vue API - Bootstrap 5 & Material Design 2.0',
-  'description' => 'API utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Vue Bootstrap 5 API',
+  'seo_title' => 'Vue API - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue API with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/api/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/api/api.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/vue/web/docs/utilities/borders/data.php
+++ en/_mdb5/vue/web/docs/utilities/borders/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Borders',
-  'seo_title' => 'Vue Borders - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Borders utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive Borders, and customization tools',
+  'subheading' => 'Vue Bootstrap 5 Borders',
+  'seo_title' => 'Vue Borders - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Borders with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/borders/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/borders/borders.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
