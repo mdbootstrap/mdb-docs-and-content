c8c8cc1a121dbfe6fc9d8bcaf97402b78ecf9a2e
unknown <a.jakubowski@mdbootstrap.com>
2021-01-29 11:51:17 +0100

    Vue utilities meta update 1/2

--- en/_mdb5/vue/web/docs/utilities/ratio/data.php
+++ en/_mdb5/vue/web/docs/utilities/ratio/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Ratio',
-  'seo_title' => 'Vue Ratio - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Ratio built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Vue Bootstrap 5 Ratio component',
+  'seo_title' => 'Vue Ratio - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Ratio built with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/ratio/',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/vue/web/docs/utilities/stretched-link/data.php
+++ en/_mdb5/vue/web/docs/utilities/stretched-link/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Stretched link',
-  'seo_title' => 'Vue Stretched link - Bootstrap 5 & Material Design 2.0',
-  'description' => 'Stretched link utilities built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Vue Bootstrap 5 Stretched link component',
+  'seo_title' => 'Vue Stretched link - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Stretched link built with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
   'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/stretched-link/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/stretched-link/stretched-link.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
