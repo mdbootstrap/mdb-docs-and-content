23c0542577201191b5a897c11a0fef1f556d3bad
filipkappa <f.kapusta@mdbootstrap.com>
2021-01-29 11:00:17 +0100

    add angular navigation data changes

--- en/_mdb5/angular/web/docs/navigation/breadcrumb/data.php
+++ en/_mdb5/angular/web/docs/navigation/breadcrumb/data.php
@@ -1,9 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Breadcrumb',
-  'seo_title' => 'Angular Breadcrumb - Bootstrap 5 & Material Design 2.0 component',
-  'description' => 'Breadcrumb built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'main_title' => 'Breadcrumbs',
+  'subheading' => 'Angular Bootstrap 5 Breadcrumbs',
+  'seo_title' => 'Angular Breadcrumbs - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Breadcrumbs built with the latest Bootstrap 5. Multiple code examples: material breadcrumbs, dynamic and more.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/navigation/breadcrumb/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/breadcrumb/breadcrumb.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/angular/web/docs/navigation/footer/data.php
+++ en/_mdb5/angular/web/docs/navigation/footer/data.php
@@ -2,8 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Footer',
-  'seo_title' => 'Angular Footer - Bootstrap 5 & Material Design 2.0 component',
-  'description' => 'Footer built with the latest Bootstrap 5 & Material Design 2.0. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'subheading' => 'Angular Bootstrap 5 Footer',
+  'seo_title' => 'Angular Footer - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Angular Footer built with the latest Bootstrap 5. Multiple code examples: material footer, bottom, fixed and more.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/navigation/footer/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/footer/footer.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
