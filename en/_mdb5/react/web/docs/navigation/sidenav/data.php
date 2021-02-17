9059f8d99918aead253c4b0f245e8f72dfeb9806
Krzysztof Wilk <xedanion16@gmail.com>
2021-02-17 16:58:11 +0100

    add sidenav docs

---
+++ en/_mdb5/react/web/docs/navigation/sidenav/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Sidenav',
+  'subheading' => 'React Bootstrap 5 Sidenav component',
+  'seo_title' => 'React Bootstrap Sidenav - examples & tutorial',
+  'description' => 'Responsive React Sidenav built with the latest Bootstrap 5. Multiple code examples: with navbar, fixed, for mobile, with filter, full height & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/navigation/sidenav/',
+  'image' => '',
+  'video' => '',
+);
