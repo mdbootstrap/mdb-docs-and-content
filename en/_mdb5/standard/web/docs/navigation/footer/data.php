ab30d04a368bf6db62c379c2d34d4f23dd6c036a
ascensus <szymanskimichal9@gmail.com>
2021-02-18 14:26:29 +0100

    Update footer docs

--- en/_mdb5/standard/web/docs/navigation/footer/data.php
+++ en/_mdb5/standard/web/docs/navigation/footer/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Footer',
   'subheading' => 'Bootstrap 5 Footer component',
   'seo_title' => 'Bootstrap Footer - examples & tutorial',
-  'description' => 'Responsive Footer built with the latest Bootstrap 5. Multiple code examples: at bottom, fixed, colors, with logo, with menu & many more',
+  'description' => 'Responsive Footer built with the latest Bootstrap 5. Multiple code examples: with copyright, at bottom, fixed, colors, links, forms and many more',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/footer/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/footer/footer.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
