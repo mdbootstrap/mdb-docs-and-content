a69ffa2893fcd0783d12b0ef59643bf34e2e2cee
idzark <a.idzikowski91@gmail.com>
2021-02-22 13:55:54 +0100

    Fix links in Angular5 docs

--- en/_mdb5/angular/web/docs/utilities/position/data.php
+++ en/_mdb5/angular/web/docs/utilities/position/data.php
@@ -5,7 +5,7 @@ $data_var = array(
   'subheading' => 'Angular Bootstrap 5 Position',
   'seo_title' => 'Angular Position - examples & tutorial. Bootstrap & Material Design',
   'description' => 'Angular Position utility with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
- 'url' => 'https://mdbootstrap.com/docs/standard/utilities/position/',
+ 'url' => 'https://mdbootstrap.com/docs/b5/angular/utilities/position/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/utilities/position/position.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
