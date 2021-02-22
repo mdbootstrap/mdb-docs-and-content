6f3d2677b273cf66d22478590a3fbc0a4c8a992c
idzark <a.idzikowski91@gmail.com>
2021-02-22 10:08:27 +0100

    Add Angular5 select and scrollbar docs

---
+++ en/_mdb5/angular/web/docs/methods/scrollbar/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Angular Scrollbar',
+  'subheading' => 'Angular Bootstrap 5 Scrollbar',
+  'seo_title' => 'Angular Bootstrap Scrollbar - examples & tutorial',
+  'description' => 'Angular Scrollbar built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/components/scrollbar/',
+  'image' => '',
+  'video' => '',
+);
