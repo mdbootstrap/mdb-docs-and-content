96e193bb26dc223508a2a45b8bfbfb5e6d926f53
Wojstan <wpstaniszewski@gmail.com>
2021-02-22 10:39:17 +0100

    add react modal docs

---
+++ en/_mdb5/react/web/docs/components/modal/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Modal',
+  'subheading' => 'React Bootstrap 5 Modal component',
+  'seo_title' => 'React Modal - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive React Modal built with the latest Bootstrap 5. Multiple code examples: popup, width, size, form, close, animation & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/modal/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/modal/modal.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
