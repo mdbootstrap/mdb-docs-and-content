7817aab024fecd6ae102d0f5058cfd3eeb29018d
Grzegorz <g.bujanski@mdbootstrap.com>
2021-02-22 11:58:57 +0100

    update docs

--- en/_mdb5/standard/web/docs/plugins/organization-chart/data.php
+++ en/_mdb5/standard/web/docs/plugins/organization-chart/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Organization Chart',
-  'subheading' => 'Bootstrap 5 Organization Chart plugin',
-  'seo_title' => 'Bootstrap Organization Chart - examples & tutorial',
-  'description' => 'Easy to use Organization Chart built with the latest Bootstrap 5. Multiple examples: organization charts',
+  'main_title' => 'Organization chart',
+  'subheading' => 'Bootstrap 5 Organization chart plugin',
+  'seo_title' => 'Bootstrap Organization chart - examples & tutorial',
+  'description' => 'Easy to use Organization chart built with the latest Bootstrap 5. Multiple examples: organization charts',
   'url' => 'https://mdbootstrap.com/docs/standard/plugins/organization-chart/',
   'image' => '',
   'video' => '',
