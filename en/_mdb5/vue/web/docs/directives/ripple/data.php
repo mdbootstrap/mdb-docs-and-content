d55f8a4b78992a6f73ca76a7d15f6b991c45994b
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2021-02-26 11:30:26 +0100

    Vue - add ripple docs

---
+++ en/_mdb5/vue/web/docs/directives/ripple/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Ripple',
+  'subheading' => 'Vue Bootstrap 5 Ripple',
+  'seo_title' => 'Vue Ripple - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Ripple built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/directives/ripple/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/methods/ripple/ripple.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
