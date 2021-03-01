51b901382309b813e42d66db0312868998ab244f
ascensus <szymanskimichal9@gmail.com>
2021-03-01 14:06:55 +0100

    Update switch docs meta data

--- en/_mdb5/standard/web/docs/forms/switch/data.php
+++ en/_mdb5/standard/web/docs/forms/switch/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Switch',
-  'subheading' => 'Bootstrap 5 Switch component',
-  'seo_title' => 'Bootstrap Switch - examples & tutorial',
-  'description' => 'Responsive Select built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'main_title' => 'Toggle Switch',
+  'subheading' => 'Bootstrap 5 Toggle Switch component',
+  'seo_title' => 'Bootstrap Toggle Switch - examples & tutorial',
+  'description' => 'Toggle Switch button built with the latest Bootstrap 5. Learn how to use on / off input and disabled state.',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/switch/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/switch/switch.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
