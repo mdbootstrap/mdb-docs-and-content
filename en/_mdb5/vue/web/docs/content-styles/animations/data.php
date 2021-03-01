f4928db62f18bdcb4c139bbf1dc97b05e82e79ee
Marcin Luczak <mbukacz@gmail.com>
2021-03-01 16:06:56 +0100

    Add MDB5 Vue Animations docs

---
+++ en/_mdb5/vue/web/docs/content-styles/animations/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Animations',
+  'subheading' => 'Vue Bootstrap 5 Animations',
+  'seo_title' => 'Vue Animations - examples & tutorial',
+  'description' => 'Vue Animations built with the latest Bootstrap 5. Multiple code examples: for sidenav, slideshow, range slider, panels & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/content-styles/animations/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/animations/animations.jpg',
+  'video' => '',
+);
