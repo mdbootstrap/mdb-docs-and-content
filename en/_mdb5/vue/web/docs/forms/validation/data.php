6c829ebf74a8ac482892aafb52a91bf7ff260333
Marcin Luczak <mbukacz@gmail.com>
2021-03-02 10:44:09 +0100

    Add MDB5 Vue Validation docs and components API sections

---
+++ en/_mdb5/vue/web/docs/forms/validation/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Validation',
+  'subheading' => 'Vue Bootstrap 5 Validation',
+  'seo_title' => 'Vue Validation - examples & tutorial',
+  'description' => 'Forms Validation built with the latest Bootstrap 5. Multiple examples, a user-friendly guide, extensive API, and customization tools',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/validation/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/validation/validation.jpg',
+  'video' => '',
+);
