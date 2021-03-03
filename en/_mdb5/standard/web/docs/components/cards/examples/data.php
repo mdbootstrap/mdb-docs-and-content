05b249b5b47c711d56e28109e61615a6750c6476
Ascensus <m.szymanski@mdbootstrap.com>
2021-03-03 09:48:31 +0100

    Add cards examples docs

---
+++ en/_mdb5/standard/web/docs/components/cards/examples/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Cards examples & customization',
+  'subheading' => 'Bootstrap 5 Card examples & customization',
+  'seo_title' => '27 Bootstrap Cards examples - various design, templates & functionalities',
+  'description' => 'A stunning collection of card templates built with the newest Bootstrap 5. Weather cards, cards with charts, animated cards & many more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/cards/examples-and-customization/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2015/08/cards.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
