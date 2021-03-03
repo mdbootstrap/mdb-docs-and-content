78e0983e2400b4c30d67633373751d624c0b38ee
Ascensus <m.szymanski@mdbootstrap.com>
2021-03-03 16:45:20 +0100

    Add background image docs

---
+++ en/_mdb5/standard/web/docs/content-styles/background-image/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Background Image',
+  'subheading' => 'Bootstrap 5 Background Image',
+  'seo_title' => 'Bootstrap 5 Background Image - examples & tutorial',
+  'description' => 'Learn how to create a responsive background image with the newest Bootstrap 5. Many examples and ready-to-use solutions',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/background-image/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2017/10/backgound-image.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
