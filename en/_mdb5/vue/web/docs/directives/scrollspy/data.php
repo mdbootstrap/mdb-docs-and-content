39f0e2290f37f3577bd12986a4b3577c52d55a3e
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2021-03-04 10:21:05 +0100

    Vue - add scrollspy, Standard - fix scrollspy demo

---
+++ en/_mdb5/vue/web/docs/directives/scrollspy/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Scrollspy',
+  'subheading' => 'Vue Bootstrap 5 Scrollspy',
+  'seo_title' => 'Vue Scrollspy - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive Vue Scrollspy built with the latest Bootstrap 5. Multiple code examples: smooth scroll, horizontal, vertical, animations, offset & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/directives/scrollspy/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/scrollspy/scrollspy.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
