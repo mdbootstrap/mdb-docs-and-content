39d6b3d7b3251dfb0744dcea5618f0ba71db5a83
Wojstan <wpstaniszewski@gmail.com>
2021-03-05 13:39:44 +0100

    add animation and scrollspy docs

---
+++ en/_mdb5/react/web/docs/content-styles/animations/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Animations',
+  'subheading' => 'React Bootstrap 5 Animations',
+  'seo_title' => 'React Animations - examples & tutorial',
+  'description' => 'React Animations built with the latest Bootstrap 5. Multiple code examples: for sidenav, slideshow, range slider, panels & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/content-styles/animations/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/animations/animations.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/react/web/docs/navigation/scrollspy/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Scrollspy',
+  'subheading' => 'React Bootstrap 5 Scrollspy component',
+  'seo_title' => 'React Scrollspy - examples & tutorial',
+  'description' => 'Responsive React Scrollspy built with the latest Bootstrap 5. Multiple code examples: smooth scroll, horizontal, vertical, animations, offset & many more',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/navigation/scrollspy/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/scrollspy/scrollspy.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
