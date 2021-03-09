0cf5dc029b82996ce6eec8b8028d78b873640f34
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2021-03-09 16:04:24 +0100

    Vue - add range docs

---
+++ en/_mdb5/vue/web/docs/forms/range/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Range',
+  'subheading' => 'Vue Bootstrap 5 Range',
+  'seo_title' => 'Vue Range - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Range built with the latest Bootstrap 5. Pick values from a range, using a customizable slider.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/forms/range/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/range/range.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
