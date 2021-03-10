29d7689b4e5fa6af33df9205ca1d2905a237fb3b
Ascensus <m.szymanski@mdbootstrap.com>
2021-03-10 10:25:58 +0100

    Add extended docs template

---
+++ en/_mdb5/standard/web/docs/extended/sidebar/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Sidebar',
+  'subheading' => 'Bootstrap 5 Sidebar component',
+  'seo_title' => 'Bootstrap Sidebar - examples & tutorial',
+  'description' => 'Responsive Sidebar built with the latest Bootstrap 5. Sidebar is an additional navigation component that provides extensive support and a clear way for navigating through complex websites with hundreds of links and subpages.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/sidebar/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
