a453150a2ce2821f2685be7f3adbbf66cd34b77a
Ascensus <m.szymanski@mdbootstrap.com>
2021-03-10 12:20:36 +0100

    Update tables examples

--- en/_mdb5/standard/web/docs/data/tables/examples/data.php
+++ en/_mdb5/standard/web/docs/data/tables/examples/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '<Name> examples & customization',
-  'subheading' => 'Bootstrap 5 <Name> examples & customization',
-  'seo_title' => '<Number> Bootstrap <Name> examples - various templates, design & functionalities',
-  'description' => 'A stunning collection of <name> templates built with the newest Bootstrap 5. ',
-  'url' => '',
-  'image' => '',
+  'main_title' => 'Tables examples & customization',
+  'subheading' => 'Bootstrap 5 Tables examples & customization',
+  'seo_title' => '10 Bootstrap Tables examples - various templates, design & functionalities',
+  'description' => 'A stunning collection of Tables templates built with the newest Bootstrap 5. Free for personal and commercial use',
+  'url' => 'https://mdbootstrap.com/docs/standard/data/tables/examples-and-customization/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2015/08/table-fb.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
