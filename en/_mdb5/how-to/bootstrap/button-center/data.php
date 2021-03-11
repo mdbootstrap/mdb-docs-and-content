78103cc7382d6839ba35f28817fa3c387d6c9af5
ascensus <szymanskimichal9@gmail.com>
2021-03-11 13:24:02 +0100

    Add How To center button page

---
+++ en/_mdb5/how-to/bootstrap/button-center/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'How to: Bootstrap button center',
+  'seo_title' => 'How to center a button in Bootstrap - code helpers',
+  'description' => 'You can align the button to the center by simply adding alignment class .text-center to the parent div.',
+  'url' => 'https://mdbootstrap.com/how-to/bootstrap/button-center/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/how-to/bootstrap/howto.jpg',
+  'video' => '',
+);
