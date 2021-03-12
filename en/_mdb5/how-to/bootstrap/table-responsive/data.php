cd33afcf393445d69075fb1587418d14c0693e47
ascensus <szymanskimichal9@gmail.com>
2021-03-12 10:33:14 +0100

    Add page how to make table responsive

---
+++ en/_mdb5/how-to/bootstrap/table-responsive/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'How to make table responsive in Bootstrap',
+  'seo_title' => 'How to make table responsive in Bootstrap - code helpers',
+  'description' => 'You can make any table responsive across all viewports by wrapping a .table with .table-responsive. class. Thanks to this it can be scrolled horizontally.',
+  'url' => 'https://mdbootstrap.com/how-to/bootstrap/table-responsive/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/how-to/bootstrap/howto.jpg',
+  'video' => '',
+);
