fb48b9aa5957a06382fcd6d09d3dff25dbb53973
ascensus <szymanskimichal9@gmail.com>
2021-03-12 10:40:51 +0100

    Update how to meta data template

--- en/_mdb5/how-to/bootstrap/_blank/data.php
+++ en/_mdb5/how-to/bootstrap/_blank/data.php
@@ -1,8 +1,8 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'How to: Bootstrap zxc',
-  'seo_title' => 'How to zxc in Bootstrap - code helpers',
+  'main_title' => 'How to ',
+  'seo_title' => 'How to  - code helpers',
   'description' => '',
   'url' => 'zxc',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/how-to/bootstrap/howto.jpg',
