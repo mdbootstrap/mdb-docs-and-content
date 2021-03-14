85a8381218920aeee349cab4b60f383a02e87639
Dawid Adach <d.adach@mdbootstrap.com>
2021-03-14 23:12:35 +0100

    Add new docs pages

---
+++ en/_mdb5/standard/web/docs/cli/database/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'CLI Databases Documentation',
+  'seo_title' => 'MDB CLI Databases Documentation',
+  'description' => 'Create, deploy and host your MDB project with a single command',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/database/',
+  'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
+  'video' => 'https://www.youtube.com/watch?v=-pIJnQQMexg',
+);
