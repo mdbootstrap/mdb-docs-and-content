a0c559bfaeafac02eef50c24803c1f3d8adf5cbe
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-16 20:36:20 +0100

    Create animations extended docs in progress

--- en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
+++ en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '<Name> examples & customization',
-  'subheading' => 'Bootstrap 5 <Name> examples & customization',
-  'seo_title' => '<Number> Bootstrap <Name> examples - various templates, design & functionalities',
-  'description' => 'A stunning collection of <name> templates built with the newest Bootstrap 5. ',
+  'main_title' => 'Animations examples & customization',
+  'subheading' => 'Bootstrap 5 Animations examples & customization',
+  'seo_title' => '10 Bootstrap Animations examples - various templates, design & functionalities',
+  'description' => 'A stunning collection of Animations templates built with the newest Bootstrap 5. ',
   'url' => '',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
