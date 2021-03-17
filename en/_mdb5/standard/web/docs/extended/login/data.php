93ae44818a0acc724b5da7829d07b809855f49cd
ascensus <szymanskimichal9@gmail.com>
2021-03-17 14:15:41 +0100

    Add extended docs login directory

---
+++ en/_mdb5/standard/web/docs/extended/login/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => '[Name]',
+  'subheading' => 'Bootstrap 5 [Name] component',
+  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
+  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/login/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
