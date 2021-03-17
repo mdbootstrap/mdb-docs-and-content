7f3501f66104aad88b40575c85a061a94bbe54dd
ascensus <szymanskimichal9@gmail.com>
2021-03-17 13:00:14 +0100

    Add timeline extended docs directory

---
+++ en/_mdb5/standard/web/docs/extended/timeline/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Timeline',
+  'subheading' => 'Bootstrap 5 Timeline component',
+  'seo_title' => 'Bootstrap Timeline - examples & tutorial',
+  'description' => 'Responsive Timelines built with the latest Bootstrap 5. Horizontal and vertical examples. Easy to use and customize.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
