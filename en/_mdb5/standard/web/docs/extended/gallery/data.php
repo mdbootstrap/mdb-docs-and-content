dd1d38df6e0d2042404e35082fc96abfa4754e57
ascensus <szymanskimichal9@gmail.com>
2021-03-18 10:29:34 +0100

    Add extended docs - gallery page

---
+++ en/_mdb5/standard/web/docs/extended/gallery/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Gallery',
+  'subheading' => 'Bootstrap 5 Gallery component',
+  'seo_title' => 'Bootstrap Gallery - examples & tutorial',
+  'description' => 'A stunning collection of responsive galleries created with the latest Bootstrap 5. Image gallery, video gallery, full-page, eCommerce, lightbox, slider, thumbnails, and many more. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/gallery/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2018/12/gallery.jpg',
+  'video' => '',
+);
