648170a43caa541f479dcb0f6787c99aeba0b717
ascensus <szymanskimichal9@gmail.com>
2021-03-18 11:13:31 +0100

    Add extended docs page - video carousel

---
+++ en/_mdb5/standard/web/docs/extended/video-carousel/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Video carousel / gallery',
+  'subheading' => 'Bootstrap 5 Video carousel / gallery',
+  'seo_title' => 'Bootstrap video carousel / gallery - examples & tutorial',
+  'description' => 'An example of responsive bootstrap carousel with videos instead of images.  Full-page video template included.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/video-carousel/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2018/12/video.jpg',
+  'video' => '',
+);
