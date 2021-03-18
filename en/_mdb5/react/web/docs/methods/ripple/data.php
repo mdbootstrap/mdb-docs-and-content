ed6c71098d410ec2deb2f758c6da8025addce0c5
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-03-18 13:14:59 +0100

    add ripple, tabs and pills docs for mdb5 react

---
+++ en/_mdb5/react/web/docs/methods/ripple/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Ripple',
+  'subheading' => 'React Bootstrap 5 Ripple',
+  'seo_title' => 'React Bootstrap Ripple - examples & tutorial',
+  'description' => 'Waves animation for the latest Bootstrap 5. Change the color of the ripple, ripple duration time, the radius of the ripple in button or image.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/methods/ripple/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/methods/ripple/ripple.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/react/web/docs/navigation/tabs/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Tabs',
+  'subheading' => 'React Bootstrap 5 Tabs component',
+  'seo_title' => 'React Bootstrap Tabs - examples & tutorial',
+  'description' => 'Responsive Tabs built with the latest Bootstrap 5. Tabs help you create panels with content that can be hidden by default and activated on click. Learn how to use tabs with switch, vertical orientation and more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/navigation/tabs/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/navigation/tabs/tabs.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
