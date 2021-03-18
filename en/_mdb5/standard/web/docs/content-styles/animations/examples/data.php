4e6251da66a424fd8799e06b07fb7e9f2685d0d5
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-18 08:51:40 +0100

    Update animation docs page

--- en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
+++ en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
@@ -5,7 +5,7 @@ $data_var = array(
   'subheading' => 'Bootstrap 5 Animations examples & customization',
   'seo_title' => '10 Bootstrap Animations examples - various templates, design & functionalities',
   'description' => 'A stunning collection of Animations templates built with the newest Bootstrap 5. ',
-  'url' => '',
-  'image' => '',
+  'url' => 'https://mdbootstrap.com/docs/standard/content-styles/animations/examples-and-customization/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap8/content-gh/en/_mdb5/standard/web/docs/content-styles/animations/examples/assets/featured.jpg',
   'video' => 'https://www.youtube.com/watch?v=Aab6byRaGf0',
 );
