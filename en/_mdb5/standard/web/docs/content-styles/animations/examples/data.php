38747d5cf12fe5ea3158039d34f2e6e8e7164139
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-18 16:05:40 +0100

    Update animations docs page

--- en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
+++ en/_mdb5/standard/web/docs/content-styles/animations/examples/data.php
@@ -4,8 +4,8 @@ $data_var = array(
   'main_title' => 'Animations examples & customization',
   'subheading' => 'Bootstrap 5 Animations examples & customization',
   'seo_title' => '10 Bootstrap Animations examples - various templates, design & functionalities',
-  'description' => 'A stunning collection of Animations templates built with the newest Bootstrap 5. ',
+  'description' => 'A stunning collection of Animations templates built with the newest Bootstrap 5. Animations on scroll, animations on click, fading animations and many more',
   'url' => 'https://mdbootstrap.com/docs/standard/content-styles/animations/examples-and-customization/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap8/content-gh/en/_mdb5/standard/web/docs/content-styles/animations/examples/assets/featured.jpg',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/animations/examples/assets/featured.jpg',
   'video' => 'https://www.youtube.com/watch?v=Aab6byRaGf0',
 );
