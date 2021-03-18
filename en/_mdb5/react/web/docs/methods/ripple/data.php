e340a87913b596861bb91cf4f2c7feb774cc2f96
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-03-18 14:25:16 +0100

    update mdb5 react docs with ripple and tabs components

--- en/_mdb5/react/web/docs/methods/ripple/data.php
+++ en/_mdb5/react/web/docs/methods/ripple/data.php
@@ -3,7 +3,7 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Ripple',
   'subheading' => 'React Bootstrap 5 Ripple',
-  'seo_title' => 'React Bootstrap Ripple - examples & tutorial',
+  'seo_title' => 'React Ripple - examples & tutorial',
   'description' => 'Waves animation for the latest Bootstrap 5. Change the color of the ripple, ripple duration time, the radius of the ripple in button or image.',
   'url' => 'https://mdbootstrap.com/docs/b5/react/methods/ripple/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/methods/ripple/ripple.jpg',
--- en/_mdb5/react/web/docs/navigation/pills/data.php
+++ en/_mdb5/react/web/docs/navigation/pills/data.php
@@ -3,7 +3,7 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Pills',
   'subheading' => 'React Bootstrap 5 Pills component',
-  'seo_title' => 'React Bootstrap Pills - examples & tutorial',
+  'seo_title' => 'React Pills - examples & tutorial',
   'description' => 'Responsive Pills built with the latest Bootstrap 5. Bootstrap navs like tabs or pills help to organize content on a page. Learn how to use pills in the navbar, in content, in cards, and with different styling options.',
   'url' => 'https://mdbootstrap.com/docs/b5/react/navigation/pills/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/navigation/pills/pills.jpg',
