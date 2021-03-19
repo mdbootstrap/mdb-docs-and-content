3381f9aabc99b4ea457f07c24b184fc5491d1d17
Dawid Wajszczuk <d.wajszczuk@mdbootstrap.com>
2021-03-19 15:41:44 +0100

    add Captcha docs

---
+++ en/_mdb5/standard/web/docs/plugins/captcha/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Captcha',
+  'subheading' => 'Bootstrap 5 Captcha plugin',
+  'seo_title' => 'Bootstrap Captcha - examples & tutorial',
+  'description' => 'Captcha plugin',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/captcha/',
+  'image' => '',
+  'video' => '',
+);
