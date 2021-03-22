9d61abba2f196dbce695da2b3e06f7fd96ed57aa
Michal Duszak <m.duszak@mdbootstrap.com>
2021-03-22 14:36:08 +0100

    add treetable docs

---
+++ en/_mdb5/standard/web/docs/plugins/treetable/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Treetable',
+  'subheading' => 'Bootstrap 5 Treetable plugin',
+  'seo_title' => 'Bootstrap Treetable - examples & tutorial',
+  'description' => 'Responsive Treetable built with the latest Bootstrap 5. Multiple examples: expand, collapse, data,',
+  'url' =>  'https://acc01.mdbootstrap.com/docs/standard/plugins/treetable/',
+  'image' => '',
+  'video' => '',
+);
