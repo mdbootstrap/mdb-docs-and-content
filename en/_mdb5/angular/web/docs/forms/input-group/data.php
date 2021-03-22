b3e45c2dab78c351c8c0415c148a5b1ae879387f
Grzegorz <g.bujanski@mdbootstrap.com>
2021-03-22 12:56:43 +0100

    add MDB5 Angular input group & range docs

---
+++ en/_mdb5/angular/web/docs/forms/input-group/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Input group',
+  'subheading' => 'Bootstrap 5 Input group component',
+  'seo_title' => 'Bootstrap Input group - examples & tutorial',
+  'description' => 'Responsive Input group built with the latest Bootstrap 5. Extends an input by adding an icon, text, button, or a button group you can place on both sides of an input.',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/input-group/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/input-group/input-group.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
