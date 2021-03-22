3f5a581e4a56eb8e0ab891ea4b29afed785262c9
idzark <a.idzikowski91@gmail.com>
2021-03-22 10:04:11 +0100

    Add Angular5 file, switch, pills and tabs docs

---
+++ en/_mdb5/angular/web/docs/navigation/pills/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Pills',
+  'subheading' => 'Angular Bootstrap 5 Pills component',
+  'seo_title' => 'Angular Bootstrap Pills - examples & tutorial',
+  'description' => 'Angular Responsive Pills built with the latest Bootstrap 5. Bootstrap navs like tabs or pills help to organize content on a page. Learn how to use pills in the navbar, in content, in cards, and with different styling options.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/navigation/pills/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/pills/pills.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
---
+++ en/_mdb5/angular/web/docs/navigation/tabs/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Tabs',
+  'subheading' => 'Angular Bootstrap 5 Tabs component',
+  'seo_title' => 'Angular Bootstrap Tabs - examples & tutorial',
+  'description' => 'Angular Responsive Tabs built with the latest Bootstrap 5. Tabs help you create panels with content that can be hidden by default and activated on click. Learn how to use tabs with switch, vertical orientation and more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/navigation/tabs/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/tabs/tabs.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
