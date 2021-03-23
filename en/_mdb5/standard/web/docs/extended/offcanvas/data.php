b370b5a594c7044f8035d8c403d220abe1f0f800
ascensus <szymanskimichal9@gmail.com>
2021-03-23 08:36:39 +0100

    Add offcanvas docs

---
+++ en/_mdb5/standard/web/docs/extended/offcanvas/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Offcanvas',
+  'subheading' => 'Bootstrap 5 Offcanvas component',
+  'seo_title' => 'Bootstrap Offcanvas - examples & tutorial',
+  'description' => 'Responsive Offcanvas built with the latest Bootstrap 5. Hidden sidebar, perfect for use in the form of additional navigation, for example in ecommerce projects or dashboards.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/offcanvas/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2015/08/sidenav-min.jpg',
+  'video' => '',
+);
