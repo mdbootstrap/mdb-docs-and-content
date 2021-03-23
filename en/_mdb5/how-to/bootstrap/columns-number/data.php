78eff099450ec383c28a8d95756e65742a53dfd3
ascensus <szymanskimichal9@gmail.com>
2021-03-23 11:29:35 +0100

    Add how to page - grid columns

--- en/_mdb5/how-to/bootstrap/columns-number/data.php
+++ en/_mdb5/how-to/bootstrap/columns-number/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'How to ',
-  'seo_title' => 'How to  - code helpers',
-  'description' => '',
-  'url' => 'zxc',
+  'main_title' => 'How many columns are in Bootstrap? ',
+  'seo_title' => 'How many columns are in Bootstrap?  - code helpers',
+  'description' => 'You can use a maximum of 12 columns in a Bootstrap grid system. See the detailed explanation.',
+  'url' => 'https://mdbootstrap.com/how-to/bootstrap/columns-number/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/how-to/bootstrap/howto.jpg',
   'video' => '',
 );
