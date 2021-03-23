9c4dcbc98922960e404e39ebbf3ba7e3320ea369
ascensus <szymanskimichal9@gmail.com>
2021-03-23 10:14:19 +0100

    Add multiselect docs

---
+++ en/_mdb5/standard/web/docs/extended/multiselect/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Multiselect',
+  'subheading' => 'Bootstrap 5 Multiselect component',
+  'seo_title' => 'Bootstrap Multiselect - examples & tutorial',
+  'description' => 'Responsive Multiselect built with the latest Bootstrap 5. Unlike a normal select, multiselect allows the user to select multiple options at once.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/multiselect/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2017/07/multiselect-fb.jpg',
+  'video' => '',
+);
