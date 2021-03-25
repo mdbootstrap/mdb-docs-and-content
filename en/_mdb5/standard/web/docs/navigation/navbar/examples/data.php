a32885e4de4bcd90349315ff0c9ce1c3a71be1e7
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-25 23:04:19 +0100

    Update navbar docs page

--- en/_mdb5/standard/web/docs/navigation/navbar/examples/data.php
+++ en/_mdb5/standard/web/docs/navigation/navbar/examples/data.php
@@ -3,8 +3,8 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Navbar examples & customization',
   'subheading' => 'Bootstrap 5 Navbar examples & customization',
-  'seo_title' => '<Number> Bootstrap Navbar examples - various templates, design & functionalities',
-  'description' => 'A stunning collection of navbar templates built with the newest Bootstrap 5. ',
+  'seo_title' => '30 Bootstrap Navbar examples - various templates, design & functionalities',
+  'description' => 'A stunning collection of Navbar templates built with the newest Bootstrap 5. Navbar YouTube, Facebook, Amazon styles, Navbar with image, carousel, Navbar light, dark, gradient, transparent and many more',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/navbar/examples-and-customization/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/navbar/examples/assets/featured.jpg',
   'video' => 'https://www.youtube.com/watch?v=G3tZUO2fAEU',
