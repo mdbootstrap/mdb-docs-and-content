0126492f1571465a7c90391c144e1bd2de223363
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-25 21:09:58 +0100

    Update navbar docs page

--- en/_mdb5/standard/web/docs/navigation/navbar/examples/data.php
+++ en/_mdb5/standard/web/docs/navigation/navbar/examples/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '<Name> examples & customization',
-  'subheading' => 'Bootstrap 5 <Name> examples & customization',
-  'seo_title' => '<Number> Bootstrap <Name> examples - various templates, design & functionalities',
-  'description' => 'A stunning collection of <name> templates built with the newest Bootstrap 5. ',
-  'url' => '',
-  'image' => '',
-  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+  'main_title' => 'Navbar examples & customization',
+  'subheading' => 'Bootstrap 5 Navbar examples & customization',
+  'seo_title' => '<Number> Bootstrap Navbar examples - various templates, design & functionalities',
+  'description' => 'A stunning collection of navbar templates built with the newest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/navigation/navbar/examples-and-customization/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/navbar/examples/assets/featured.jpg',
+  'video' => 'https://www.youtube.com/watch?v=G3tZUO2fAEU',
 );
