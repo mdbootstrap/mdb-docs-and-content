713d72b1c257c97b8e7688e148b5e0c34d2404ae
ascensus <szymanskimichal9@gmail.com>
2021-03-29 14:18:28 +0200

    Add testimonial slider docs

---
+++ en/_mdb5/standard/web/docs/extended/testimonial-slider/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Testimonial Slider',
+  'subheading' => 'Bootstrap 5 Testimonial Slider component',
+  'seo_title' => 'Bootstrap Testimonial Slider - examples & tutorial',
+  'description' => 'Responsive Testimonial Slider built with the latest Bootstrap 5. See examples of the combination of Bootstrap carousel with testimonials.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/testimonial-slider/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
