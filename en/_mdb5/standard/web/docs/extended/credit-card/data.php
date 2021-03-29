c095f103db0189771418e27816a8afa9502da881
ascensus <szymanskimichal9@gmail.com>
2021-03-29 14:47:33 +0200

    Add credit card form docs

---
+++ en/_mdb5/standard/web/docs/extended/credit-card/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Credit card form',
+  'subheading' => 'Bootstrap 5 Credit Card Form component',
+  'seo_title' => 'Bootstrap Credit Card Form - examples & tutorial',
+  'description' => 'Responsive Credit Card Form built with the latest Bootstrap 5. Component to be used in eCommerce projects and checkout pages.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/credit-card/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
