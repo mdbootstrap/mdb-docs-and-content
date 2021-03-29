53cf160aa065934afe90d19467a96b84054be4e5
ascensus <szymanskimichal9@gmail.com>
2021-03-29 13:21:31 +0200

    Add nested dropdown docs

---
+++ en/_mdb5/standard/web/docs/extended/dropdown-multilevel/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Nested Dropdown',
+  'subheading' => 'Bootstrap 5 nested Dropdown component',
+  'seo_title' => 'Bootstrap nested (multilevel) Dropdown - examples & tutorial',
+  'description' => 'Nested dropdowns built with the latest Bootstrap 5. Examples with navbars and buttons. See how to add extra level submenus.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/dropdown-multilevel/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
