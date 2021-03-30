9979811a6d5a7d728b773719e82f6951361816e8
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-30 09:34:51 +0200

    Create examples and customization forms docs page

---
+++ en/_mdb5/standard/web/docs/forms/overview/examples/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Forms examples & customization',
+  'subheading' => 'Bootstrap 5 Forms examples & customization',
+  'seo_title' => '7 responsive Bootstrap Forms examples - various templates, design & functionalities',
+  'description' => 'A stunning collection of forms templates built with the newest Bootstrap 5. Free for personal and commercial use',
+  'url' => 'https://mdbootstrap.com/docs/standard/forms/overview/examples-and-customization/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/overview/examples/assets/featured.jpg',
+  'video' => '',
+);
