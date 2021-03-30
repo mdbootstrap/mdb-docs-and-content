26100196d195e782008d7356fb5a7fd07b3addda
ascensus <szymanskimichal9@gmail.com>
2021-03-30 09:00:41 +0200

    Add media object docs

---
+++ en/_mdb5/standard/web/docs/extended/media-object/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Media object',
+  'subheading' => 'Bootstrap 5 Media object component',
+  'seo_title' => 'Bootstrap 5 Media object - examples & tutorial',
+  'description' => 'Responsive Media objects built with the latest Bootstrap 5. Dedicated for highly repetitive components like blog comments, tweets, and the like.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/media-object/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/media-object/featured.jpg',
+  'video' => '',
+);
