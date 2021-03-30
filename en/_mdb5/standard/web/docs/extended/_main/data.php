cd7b04f06fd587f20a58f9ab65fc8022a00d7801
ascensus <szymanskimichal9@gmail.com>
2021-03-30 11:52:29 +0200

    Add extended docs list

---
+++ en/_mdb5/standard/web/docs/extended/_main/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Extended documentation',
+  'subheading' => 'Extended documentation',
+  'seo_title' => 'MDBootstrap Extended documentation',
+  'description' => 'Extended documentation with experimental components and functionalities.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'video' => '',
+);
