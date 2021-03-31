f501f3f332700d3656208cb1422607d9770a1099
ascensus <szymanskimichal9@gmail.com>
2021-03-31 08:09:23 +0200

    Add drawers docs

---
+++ en/_mdb5/standard/web/docs/extended/drawer/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Drawer',
+  'subheading' => 'Bootstrap 5 Drawer component',
+  'seo_title' => 'Bootstrap Drawer - examples & tutorial',
+  'description' => 'Responsive Drawer built with the latest Bootstrap 5. Drawer allows you to display additional content within the boundaries of a given component (for example navigation links), by default invisible and displayed after clicking the trigger.',
+  'url' => 'https://mdbootstrap.com/docs/standard/navigation/drawer/',
+  'image' => '',
+  'video' => '',
+);
