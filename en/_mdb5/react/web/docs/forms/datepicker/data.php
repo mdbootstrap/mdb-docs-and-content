1a2c05ff8aa0bdd1c396366a543b9485e6c05b42
Wojciech Staniszewski <w.staniszewski@mdbootstrap.com>
2021-03-31 14:22:34 +0200

    add datepicker docs

---
+++ en/_mdb5/react/web/docs/forms/datepicker/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Datepicker',
+  'subheading' => 'React Bootstrap 5 Datepicker component',
+  'seo_title' => 'React Datepicker - examples & tutorial',
+  'description' => 'Responsive Datepicker built with the latest Bootstrap 5. A plugin that adds date picker functionality without custom JS code. It supports internationalization, different formats & customization options.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/forms/datepicker/',
+  'image' => '',
+  'video' => '',
+);
