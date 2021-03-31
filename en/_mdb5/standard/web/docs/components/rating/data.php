5e4e1df0c1f707776b2f3e2fc3feff356faee783
ascensus <szymanskimichal9@gmail.com>
2021-03-31 07:28:43 +0200

    Update rating meta data

--- en/_mdb5/standard/web/docs/components/rating/data.php
+++ en/_mdb5/standard/web/docs/components/rating/data.php
@@ -2,9 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Rating',
-  'subheading' => 'Bootstrap 5 Rating component',
-  'seo_title' => 'Bootstrap Rating - examples & tutorial',
-  'description' => 'Responsive rating built with the latest Bootstrap 5. Rating provides insight into others opinions and experiences with a product. Use stars or other custom symbols (i.e. smiling faces)',
+  'subheading' => 'Bootstrap 5 Star Rating component',
+  'seo_title' => 'Bootstrap Star Rating - examples & tutorial',
+  'description' => 'Responsive star rating built with the latest Bootstrap 5. Rating provides insight into others opinions and experiences with a product. Use stars or other custom symbols (i.e. smiling faces)',
   'url' => 'https://mdbootstrap.com/docs/standard/components/rating/',
   'image' => '',
   'video' => '',
