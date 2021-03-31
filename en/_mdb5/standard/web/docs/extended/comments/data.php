06b6b9643afd097a1effd7a351cc9857107ca959
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-03-31 20:55:46 +0200

    Create comments sections finished

--- en/_mdb5/standard/web/docs/extended/comments/data.php
+++ en/_mdb5/standard/web/docs/extended/comments/data.php
@@ -4,8 +4,8 @@ $data_var = array(
   'main_title' => 'Comments',
   'subheading' => 'Bootstrap 5 Comments component',
   'seo_title' => 'Bootstrap Comments - examples & tutorial',
-  'description' => 'Responsive Comments templates built with the latest Bootstrap 5. ',
+  'description' => 'Responsive Comments templates built with the latest Bootstrap 5.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/comments/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/comments/assets/featured.jpg',
   'video' => '',
 );
