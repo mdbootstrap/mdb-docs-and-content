f07e13ac47e88ed9c57e0336aa768ce752cd8821
ascensus <szymanskimichal9@gmail.com>
2021-04-01 11:54:14 +0200

    Update thumbnails carousel docs

--- en/_mdb5/standard/web/docs/extended/carousel-with-thumbnails/data.php
+++ en/_mdb5/standard/web/docs/extended/carousel-with-thumbnails/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Bootstrap Carousel with Thumbnails',
-  'subheading' => 'Bootstrap 5 Carousel with Thumbnails component',
-  'seo_title' => 'Bootstrap Carousel with Thumbnails - examples & tutorial',
-  'description' => 'Responsive Carousel with Thumbnails built with the latest Bootstrap 5. An improved version of the standard bootstrap carousel additionally equipped with thumbnails.',
+  'main_title' => 'Bootstrap Carousel Slider with Thumbnails',
+  'subheading' => 'Bootstrap 5 Carousel Slider with Thumbnails component',
+  'seo_title' => 'Bootstrap Carousel Slider with Thumbnails - examples & tutorial',
+  'description' => 'Responsive Carousel Slider with Thumbnails built with the latest Bootstrap 5. An improved version of the standard bootstrap carousel image gallery additionally equipped with thumbnails. Vertical example included.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/carousel-with-thumbnails/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/carousel-with-thumbnails/featured.jpg',
   'video' => '',
