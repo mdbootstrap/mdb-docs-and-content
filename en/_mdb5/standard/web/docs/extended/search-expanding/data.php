5f81d30e65e9e5afd4da903e346105a608d110a6
ascensus <szymanskimichal9@gmail.com>
2021-04-01 13:11:47 +0200

    Add expanding search bar docs

---
+++ en/_mdb5/standard/web/docs/extended/search-expanding/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Expanding Search Bar',
+  'subheading' => 'Bootstrap 5 Expanding Search Bar component',
+  'seo_title' => 'Bootstrap Expanding Search Bar - examples & tutorial',
+  'description' => 'Responsive Expanding Search Bar built with Bootstrap 5. Examples with the trigger on click and on hover.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended//featured.jpg',
+  'video' => '',
+);
