2dc7b6951a60c5d1b047f02079c2736be8b8ed1c
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-01 14:16:59 +0200

    Create testimonials extended docs page in progress

--- en/_mdb5/standard/web/docs/extended/testimonials/data.php
+++ en/_mdb5/standard/web/docs/extended/testimonials/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'main_title' => 'Testimonials',
+  'subheading' => 'Bootstrap 5 Testimonials component',
+  'seo_title' => 'Bootstrap Testimonials - examples & tutorial',
+  'description' => 'Responsive Testimonials built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/testimonials/',
+  'image' => '',
   'video' => '',
 );
