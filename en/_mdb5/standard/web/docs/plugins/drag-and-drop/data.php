6ff83e8841a63493813f43a60df4487d181da950
ascensus <szymanskimichal9@gmail.com>
2021-04-01 12:46:43 +0200

    Update drag and drop meta data

--- en/_mdb5/standard/web/docs/plugins/drag-and-drop/data.php
+++ en/_mdb5/standard/web/docs/plugins/drag-and-drop/data.php
@@ -3,8 +3,8 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Drag and drop',
   'subheading' => 'Bootstrap 5 Drag and drop plugin',
-  'seo_title' => 'Bootstrap Drag and drop - examples & tutorial',
-  'description' => 'Responsive Drag and Drop plugin built with the latest Bootstrap 5. Customization examples such as custom container, delay, disable, scrolling, and many more.',
+  'seo_title' => 'Bootstrap Drag and drop (draggable & sortable) - examples & tutorial',
+  'description' => 'Drag and Drop plugin built with Bootstrap 5. Draggable list, cards, tables, grid, buttons. Sort, copy, scroll, disable, delay, nested & other options. ',
   'url' => 'https://mdbootstrap.com/docs/standard/plugins/drag-and-drop/',
   'image' => '',
   'video' => '',
