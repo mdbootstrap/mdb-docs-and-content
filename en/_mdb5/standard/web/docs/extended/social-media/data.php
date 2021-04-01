76e1012447aae6cce251717cca6cc0106d737304
ascensus <szymanskimichal9@gmail.com>
2021-04-01 10:07:16 +0200

    Add social media docs

---
+++ en/_mdb5/standard/web/docs/extended/social-media/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Social Media icons & buttons',
+  'subheading' => 'Bootstrap 5 Social Media icons & buttons',
+  'seo_title' => 'Bootstrap Social Media icons & buttons - examples & tutorial',
+  'description' => 'Social Media icons & buttons built with Bootstrap 5. Facebook, Twitter, Google, Instagram, LinkedIn, Pinterest, YouTube, GitHub, WhatsApp, Slack, Reddit & more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/social-media/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2015/09/social-buttons-min.jpg',
+  'video' => '',
+);
