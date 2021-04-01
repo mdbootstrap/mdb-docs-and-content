71447355c2549085c00217831a57d660baba8713
ascensus <szymanskimichal9@gmail.com>
2021-04-01 11:36:08 +0200

    Update back to top button meta data

--- en/_mdb5/standard/web/docs/extended/back-to-top/data.php
+++ en/_mdb5/standard/web/docs/extended/back-to-top/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Back To Top button',
-  'subheading' => 'Bootstrap 5 Back To Top button component',
-  'seo_title' => 'Bootstrap Back To Top button - examples & tutorial',
-  'description' => 'Back To Top button built with Bootstrap 5. Learn how to create a button that appears when you start scrolling and, when clicked, takes you to the top of the page.',
+  'main_title' => 'Scroll Back To Top button',
+  'subheading' => 'Bootstrap 5 Scroll Back To Top button component',
+  'seo_title' => 'Bootstrap Scroll Back To Top button - examples & tutorial',
+  'description' => 'Scroll Back To Top button built with Bootstrap 5. Learn how to create a button that appears when you start scrolling and, when clicked, takes you to the top of the page.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/back-to-top/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/back-to-top/featured.jpg',
   'video' => '',
