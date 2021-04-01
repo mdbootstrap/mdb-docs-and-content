8990b67f5a9a3c3c7ef5bba1eb30558c2f8c3814
ascensus <szymanskimichal9@gmail.com>
2021-04-01 13:41:27 +0200

    Update ecommerce blocks meta data

--- en/_mdb5/standard/web/docs/design-blocks/ecommerce/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/data.php
@@ -2,8 +2,8 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'eCommerce blocks',
-  'seo_title' => 'eCommerce blocks - Bootstrap 5 & Material Design 2.0 design blocks',
-  'description' => 'Responsive eCommerce design blocks built with the latest Bootstrap 5. built your own online store using ready-made sections such as lists, products, store home page, and many more.',
+  'seo_title' => 'eCommerce blocks & product grid - examples & design blocks',
+  'description' => 'Responsive eCommerce design blocks & product grids built with Bootstrap 5. Built your own online store using ready-made sections such as lists, products, store home page, and many more.',
   'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
