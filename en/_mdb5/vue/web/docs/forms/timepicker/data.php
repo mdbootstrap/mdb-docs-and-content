1bd889211eb6acda5f0e57186f6a5e7594c390ec
Marcin Luczak <mbukacz@gmail.com>
2021-04-02 11:30:48 +0200

    Add MDB5 Vue Timepicker docs

---
+++ en/_mdb5/vue/web/docs/forms/timepicker/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Timepicker',
+  'subheading' => 'Vue Bootstrap 5 Timepicker component',
+  'seo_title' => 'Vue Bootstrap Timepicker - examples & tutorial',
+  'description' => 'Responsive time picker built with the latest Bootstrap 5 and Vue 3. Select and set a specific time value from a pre-determined set in a popup panel.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/forms/timepicker/',
+  'image' => '',
+  'video' => '',
+);
