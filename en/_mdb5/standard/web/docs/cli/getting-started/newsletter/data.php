73a118591243e5cf34dfb2dbeb7b870d1bc9deb0
Dawid Adach <d.adach@mdbootstrap.com>
2021-04-02 08:56:38 +0200

    Add new GS pages

--- en/_mdb5/standard/web/docs/cli/getting-started/newsletter/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/newsletter/data.php
@@ -1,8 +1,8 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'MDB GO Getting Started - MDB',
-  'seo_title' => 'MDB GO Getting Started - MDB',
+  'main_title' => 'PHP Newsletter',
+  'seo_title' => 'PHP Newsletter',
   'description' => 'Create, deploy and host your MDB project with a single command',
   'url' => 'https://mdbootstrap.com/docs/standard/cli/about/',
   'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
--- en/_mdb5/standard/web/docs/cli/getting-started/node-chat/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/node-chat/data.php
@@ -1,8 +1,8 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'MDB GO Getting Started - Node/SocketIO chat app',
-  'seo_title' => 'MDB GO Getting Started - Node/SocketIO chat app',
+  'main_title' => 'Node/SocketIO chat app',
+  'seo_title' => 'Node/SocketIO chat app',
   'description' => 'Create, deploy and host your MDB project with a single command',
   'url' => 'https://mdbootstrap.com/docs/standard/cli/about/',
   'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
