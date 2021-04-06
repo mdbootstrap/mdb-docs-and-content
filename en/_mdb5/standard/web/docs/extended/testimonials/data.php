2c76151f0cbb9236c3f801925d0e928308f398a1
ascensus <szymanskimichal9@gmail.com>
2021-04-06 12:44:50 +0200

    Update testimonials meta data

--- en/_mdb5/standard/web/docs/extended/testimonials/data.php
+++ en/_mdb5/standard/web/docs/extended/testimonials/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Testimonials',
-  'subheading' => 'Bootstrap 5 Testimonials component',
-  'seo_title' => 'Bootstrap Testimonials - examples & tutorial',
-  'description' => 'Responsive Testimonials built with Bootstrap 5. Testimonials slider & carousel, testimonials with star ratings, with avatars, with background images, with cards, with gradients & many more examples.',
+  'main_title' => 'Testimonials / Reviews',
+  'subheading' => 'Bootstrap 5 Testimonials / Reviews component',
+  'seo_title' => 'Bootstrap Testimonials / Reviews - examples & tutorial',
+  'description' => 'Responsive Testimonials / Reviews built with Bootstrap 5. Testimonials slider & carousel, testimonials with star ratings, with avatars, with background images, with cards, with gradients & many more examples.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/testimonials/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/testimonials/assets/featured.jpg',
   'video' => '',
