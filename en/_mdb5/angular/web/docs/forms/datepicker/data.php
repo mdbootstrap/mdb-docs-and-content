384c33ed64df16c7115ce40b931316a2bdf94288
idzark <a.idzikowski91@gmail.com>
2021-04-06 12:05:07 +0200

    Add Angular5 datepicker docs

---
+++ en/_mdb5/angular/web/docs/forms/datepicker/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Datepicker',
+  'subheading' => 'Angular Bootstrap 5 Datepicker',
+  'seo_title' => 'Angular Bootstrap Datepicker - examples & tutorial',
+  'description' => 'Angular Responsive Datepicker built with the latest Bootstrap 5. A plugin that adds date picker functionality without custom JS code. It supports internationalization, different formats & customization options.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/datepicker/',
+  'image' => '',
+  'video' => '',
+);
