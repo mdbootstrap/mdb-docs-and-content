5cfb2120925be4eb9d2b878c2fa5969190feb6aa
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-04-06 11:18:05 +0200

    add mdb5 react timepicker docs

---
+++ en/_mdb5/react/web/docs/forms/timepicker/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Timepicker',
+  'subheading' => 'React Bootstrap 5 Timepicker component',
+  'seo_title' => 'React Timepicker - examples & tutorial',
+  'description' => 'Responsive time picker built with the latest Bootstrap 5. Select and set a specific time value from a pre-determined set in a popup panel.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/forms/timepicker/',
+  'image' => '',
+  'video' => '',
+);
