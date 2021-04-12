08d2979a2dba9bbbf32eed136584d975242cd85c
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-12 18:43:05 +0200

    Add fixes to the documentation

---
+++ en/_mdb5/standard/web/docs/content-styles/reboot/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Reboot',
+  'subheading' => 'Bootstrap 5 Reboot',
+  'seo_title' => 'Bootstrap Reboot - examples & tutorial',
+  'description' => 'Bootstrap 5 typography is the style and appearance of headings, lists, body text and basic paragraphs. See 14 examples with detailed descriptions.',
+ 'url' => 'https://mdbootstrap.com/docs/standard/content-styles/typography/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/content-styles/typography/typography.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
