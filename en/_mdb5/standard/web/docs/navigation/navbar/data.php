2429b36227a812f54f99bccec628bff1ef89118c
ascensus <szymanskimichal9@gmail.com>
2021-04-12 14:01:02 +0200

    Update navbar meta data

--- en/_mdb5/standard/web/docs/navigation/navbar/data.php
+++ en/_mdb5/standard/web/docs/navigation/navbar/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Navbar',
   'subheading' => 'Bootstrap 5 Navbar component',
   'seo_title' => 'Bootstrap Navbar - examples & tutorial',
-  'description' => 'Navbar for the latest Bootstrap 5. A navigation bar is a menu fixed to the top of the page. Use it with dropdowns, text links, or buttons. Learn how to add a logo and make your navbar responsive.',
+  'description' => 'Responsive menu built with Bootstrap 5. Examples with logo, dropdown, collapse & hamburger icon. Alignment to the left, right or center. Fixed top or bottom position. ',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/navbar/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/navbar/navbar.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
