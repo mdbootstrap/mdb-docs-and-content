755ff6059b81bdc5fd04565dd939ee69668c3343
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-13 23:10:04 +0200

    Create payment forms extended docs page

--- en/_mdb5/standard/web/docs/extended/payment-forms/data.php
+++ en/_mdb5/standard/web/docs/extended/payment-forms/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Payment Form',
-  'subheading' => 'Bootstrap 5 Payment Form component',
-  'seo_title' => 'Bootstrap Payment Form - examples & tutorial',
-  'description' => 'Responsive Payment Form built with the latest Bootstrap 5. ',
+  'main_title' => 'Payment Forms',
+  'subheading' => 'Bootstrap 5 Payment Forms component',
+  'seo_title' => 'Bootstrap Payment Forms - examples & tutorial',
+  'description' => 'Responsive Payment Forms built with the latest Bootstrap 5. ',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/payment-forms/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/payment-forms/assets/featured.jpg',
   'video' => '',
