e87041e62f519b42dbb4c3d90e3d28a08f007705
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-14 21:20:15 +0200

    Create product cards extended docs page

--- en/_mdb5/standard/web/docs/extended/product-cards/data.php
+++ en/_mdb5/standard/web/docs/extended/product-cards/data.php
@@ -6,6 +6,6 @@ $data_var = array(
   'seo_title' => 'Bootstrap Product Cards - examples & tutorial',
   'description' => 'Responsive Product Cards built with the latest Bootstrap 5. ',
   'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/payment-forms/assets/featured.jpg',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/product-cards/assets/featured.jpg',
   'video' => '',
 );
