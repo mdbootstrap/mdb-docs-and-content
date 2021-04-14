02449ce1cc35c70b23493a91c7f093c78ed26dc5
ascensus <szymanskimichal9@gmail.com>
2021-04-14 13:25:32 +0200

    Update payment forms docs

--- en/_mdb5/standard/web/docs/extended/payment-forms/data.php
+++ en/_mdb5/standard/web/docs/extended/payment-forms/data.php
@@ -2,10 +2,10 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Payment Forms',
-  'subheading' => 'Bootstrap 5 Payment Forms component',
+  'subheading' => 'Bootstrap 5 Payment Forms',
   'seo_title' => 'Bootstrap Payment Forms - examples & tutorial',
-  'description' => 'Responsive Payment Forms built with the latest Bootstrap 5. ',
+  'description' => 'Responsive Payment Forms built with the latest Bootstrap 5. Credit card, PayPal, Stripe, eCommerce checkout and many more examples.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/payment-forms/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/payment-forms/assets/featured.jpg',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/payment-forms/assets/featured.png',
   'video' => '',
 );
