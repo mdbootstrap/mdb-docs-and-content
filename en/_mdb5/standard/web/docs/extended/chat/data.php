01897df68ca7cf9c602170ef7aa690096f5d3699
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-15 20:25:00 +0200

    Create bootstrap chat extended docs page

--- en/_mdb5/standard/web/docs/extended/chat/data.php
+++ en/_mdb5/standard/web/docs/extended/chat/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'main_title' => 'Chat',
+  'subheading' => 'Bootstrap 5 Chat component',
+  'seo_title' => 'Bootstrap Chat - examples & tutorial',
+  'description' => 'Responsive Chat built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/chat/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/chat/assets/featured.jpg',
   'video' => '',
 );
