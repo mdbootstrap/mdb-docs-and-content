c61526485cba59c856ce8ec8785503a85a2d7472
ascensus <szymanskimichal9@gmail.com>
2021-04-15 17:56:30 +0200

    Update product cards docs

--- en/_mdb5/standard/web/docs/extended/product-cards/data.php
+++ en/_mdb5/standard/web/docs/extended/product-cards/data.php
@@ -2,10 +2,10 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Product Cards',
-  'subheading' => 'Bootstrap 5 Product Cards component',
+  'subheading' => 'Bootstrap 5 Product Cards',
   'seo_title' => 'Bootstrap Product Cards - examples & tutorial',
-  'description' => 'Responsive Product Cards built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'description' => 'Responsive Product Cards built with the latest Bootstrap 5. Lots of examples of different designs for eCommerce components',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/product-cards/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/product-cards/assets/featured.jpg',
   'video' => '',
 );
