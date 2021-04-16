2215e2122b6900dfa1060dc7d758e4ad84cc0632
Marcin Luczak <mbukacz@gmail.com>
2021-04-16 12:24:29 +0200

    Add MDB5 Vue Toasts and Alerts docs

---
+++ en/_mdb5/vue/web/docs/components/toasts/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Toasts',
+  'subheading' => 'Vue Bootstrap 5 Toast component',
+  'seo_title' => 'Vue Bootstrap Toasts - examples & tutorial',
+  'description' => 'Bootstrap Toast component is a non-disruptive message in the corner of the interface.  It provides quick "at-a-glance" feedback on the outcome of an action.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/components/toasts/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/toasts/toasts.jpg',
+  'video' => '',
+);
