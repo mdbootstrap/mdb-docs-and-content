ebdba5ea78e670786db5078dbb513f245b117bc5
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-16 11:19:33 +0200

    Create weather extended docs page

--- en/_mdb5/standard/web/docs/extended/weather/data.php
+++ en/_mdb5/standard/web/docs/extended/weather/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'main_title' => 'Weather',
+  'subheading' => 'Bootstrap 5 Weather component',
+  'seo_title' => 'Bootstrap Weather - examples & tutorial',
+  'description' => 'Responsive Weather built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/weather/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
   'video' => '',
 );
