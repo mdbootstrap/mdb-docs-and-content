84f42d30e669c0dfb5c5863ee13e2b7f714e2e9b
idzark <a.idzikowski91@gmail.com>
2021-04-19 11:02:22 +0200

    Add Angular5 carousel docs

---
+++ en/_mdb5/angular/web/docs/components/carousel/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Carousel',
+  'subheading' => 'Angular Bootstrap 5 Carousel component',
+  'seo_title' => 'Angular Bootstrap Carousel - examples & tutorial',
+  'description' => 'Responsive Angular carousel built with the latest Bootstrap 5. Carousel is a slideshow cycling through different elements such as photos, videos, or text. Many examples and easy tutorials.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/components/carousel/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/carousel/carousel.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
