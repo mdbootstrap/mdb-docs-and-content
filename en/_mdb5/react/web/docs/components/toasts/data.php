df95d4e70b68718786f20ed27e6fe63b07caf685
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-04-19 14:37:00 +0200

    add mdb5 react alerts and toasts docs

---
+++ en/_mdb5/react/web/docs/components/toasts/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Toasts',
+  'subheading' => 'React Bootstrap 5 Toast component',
+  'seo_title' => 'React Toasts - examples & tutorial',
+  'description' => 'React Toast component is a non-disruptive message in the corner of the interface.  It provides quick "at-a-glance" feedback on the outcome of an action.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/toasts/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/react/web/docs/components/toasts/toasts.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
