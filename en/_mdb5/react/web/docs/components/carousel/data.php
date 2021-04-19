3d08f715c30f18efac1910a208339bce4ee4654d
Wojstan <wpstaniszewski@gmail.com>
2021-04-19 11:12:26 +0200

    add react carousel docs

---
+++ en/_mdb5/react/web/docs/components/carousel/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Carousel',
+  'subheading' => 'React Bootstrap 5 Carousel component',
+  'seo_title' => 'React Carousel - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Responsive React Carousel built with the latest Bootstrap 5. Multiple code examples: notification badge, app badge,  navigation tab & many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/carousel/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/carousel/carousel.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
