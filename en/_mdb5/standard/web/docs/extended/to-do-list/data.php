e7cce8607efef0bc223a527224071a6e77046fe6
ascensus <szymanskimichal9@gmail.com>
2021-04-21 13:10:44 +0200

    Update todolist descirpion

--- en/_mdb5/standard/web/docs/extended/to-do-list/data.php
+++ en/_mdb5/standard/web/docs/extended/to-do-list/data.php
@@ -3,8 +3,8 @@ global $data_var;
 $data_var = array(
   'main_title' => 'To Do List',
   'subheading' => 'Bootstrap 5 To Do List component',
-  'seo_title' => 'Bootstrap To Do List - examples & tutorial',
-  'description' => 'Responsive To Do List built with the latest Bootstrap 5. ',
+  'seo_title' => 'Bootstrap To Do List - examples & templates',
+  'description' => 'Responsive To Do Lists built with the latest Bootstrap 5. Various variants of design and functionality. ',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/to-do-list/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/to-do-list/assets/featured.png',
   'video' => '',
