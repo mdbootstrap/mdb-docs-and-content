f8821ef8dd73d8d9ed617358f20982846bfcef3d
ascensus <szymanskimichal9@gmail.com>
2021-04-21 20:19:57 +0200

    Add new extended docs

---
+++ en/_mdb5/standard/web/docs/extended/mega-menu/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => '[Name]',
+  'subheading' => 'Bootstrap 5 [Name] component',
+  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
+  'description' => 'Responsive [Name] built with Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended//featured.jpg',
+  'video' => '',
+);
