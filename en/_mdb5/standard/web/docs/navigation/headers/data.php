54a279eac5815843b45f9046fa4338f1c5876e1b
ascensus <szymanskimichal9@gmail.com>
2021-04-22 08:56:45 +0200

    Update navigation meta data

--- en/_mdb5/standard/web/docs/navigation/headers/data.php
+++ en/_mdb5/standard/web/docs/navigation/headers/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Headers',
   'subheading' => 'Bootstrap 5 Header component',
   'seo_title' => 'Bootstrap Headers - examples & tutorial',
-  'description' => 'Responsive Headers built with the latest Bootstrap 5. Headers are navigation compositions that extend standard navbar functionalities.',
+  'description' => 'Responsive Headers built with Bootstrap 5. Examples of fixed header, header sticky on scroll, with background image, jumbotron & more.',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/headers/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/headers/headers.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
--- en/_mdb5/standard/web/docs/navigation/scrollspy/data.php
+++ en/_mdb5/standard/web/docs/navigation/scrollspy/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Scrollspy',
   'subheading' => 'Bootstrap 5 Scrollspy component',
   'seo_title' => 'Bootstrap Scrollspy - examples & tutorial',
-  'description' => 'Responsive Scrollspy built with the latest Bootstrap 5. Highlight where the user is on a page by updating links in navigation based on scroll postion.',
+  'description' => 'Responsive Scrollspy built with Bootstrap 5. Highlight where the user is on a page by updating links in navigation based on scroll postion. Works perfectly with smooth scroll.',
   'url' => 'https://mdbootstrap.com/docs/standard/navigation/scrollspy/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/navigation/scrollspy/scrollspy.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
