cecc7074fe392a0bde7d4ecbcbc85aaed72bcc1f
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-23 14:13:34 +0200

    Create profiles extended docs page in progress

--- en/_mdb5/standard/web/docs/extended/profiles/data.php
+++ en/_mdb5/standard/web/docs/extended/profiles/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'main_title' => 'Profiles',
+  'subheading' => 'Bootstrap 5 Profiles component',
+  'seo_title' => 'Bootstrap Profiles - examples & tutorial',
+  'description' => 'Responsive Profiles built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/profiles/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/profiles/assets/featured.jpg',
   'video' => '',
 );
