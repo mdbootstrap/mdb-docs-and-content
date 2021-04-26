35597a5694af1107d3f942794a8eb47f694234f3
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-26 10:20:19 +0200

    Create login forms extended docs page in progress

--- en/_mdb5/standard/web/docs/extended/login/data.php
+++ en/_mdb5/standard/web/docs/extended/login/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
+  'main_title' => 'Login form',
+  'subheading' => 'Bootstrap 5 Login form component',
+  'seo_title' => 'Bootstrap Login form - examples & tutorial',
+  'description' => 'Responsive Login form built with the latest Bootstrap 5. ',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/login/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
   'video' => '',
