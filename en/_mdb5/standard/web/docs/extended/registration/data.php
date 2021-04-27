76e6b66756e0bdaada8d6f98cd5ad1fa43168a1c
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-27 10:42:38 +0200

    Create registration form extended docs page

--- en/_mdb5/standard/web/docs/extended/registration/data.php
+++ en/_mdb5/standard/web/docs/extended/registration/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
+  'main_title' => 'Registration form',
+  'subheading' => 'Bootstrap 5 Registration form component',
+  'seo_title' => 'Bootstrap Registration form - examples & tutorial',
+  'description' => 'Responsive Registration form built with Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/registration/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended//featured.jpg',
   'video' => '',
 );
