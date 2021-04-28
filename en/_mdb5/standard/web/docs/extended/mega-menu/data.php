9f5b9c33c60b371ad6498c9e9169f7b7235f8d44
ascensus <szymanskimichal9@gmail.com>
2021-04-28 08:26:05 +0200

    Add mega menu docs

--- en/_mdb5/standard/web/docs/extended/mega-menu/data.php
+++ en/_mdb5/standard/web/docs/extended/mega-menu/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended//featured.jpg',
+  'main_title' => 'Mega Menu',
+  'subheading' => 'Bootstrap 5 Mega Menu component',
+  'seo_title' => 'Bootstrap Mega Menu - examples & tutorial',
+  'description' => 'Responsive Mega Menu built with Bootstrap 5. Examples of dropdown on click and on hover. Templates with grid, images, links, lists and more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/mega-menu/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/mega-menu/featured.jpg',
   'video' => '',
 );
