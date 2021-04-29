587d096b68baff7b1be2584607b766894d3d6078
Wojstan <wpstaniszewski@gmail.com>
2021-04-29 16:14:25 +0200

    add react datatables docs

---
+++ en/_mdb5/react/web/docs/data/datatables/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Datatables',
+  'subheading' => 'React Bootstrap 5 Datatables',
+  'seo_title' => 'React Datatables - examples & tutorial',
+  'description' => 'React Datatables built with the latest Bootstrap 5. Advanced customization options for tables like searching, sorting, and pagination.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/data/datatables/',
+  'image' => '',
+  'video' => '',
+);
