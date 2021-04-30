bb116bdb40ae528c1d5e6c719115585024e85b9c
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-04-30 11:06:36 +0200

    add mdb5 react charts docs

---
+++ en/_mdb5/react/web/docs/data/charts/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Charts',
+  'subheading' => 'React Bootstrap 5 Charts',
+  'seo_title' => 'React Charts - examples & tutorial',
+  'description' => 'Responsive React Charts built with the latest Bootstrap 5. Different types of data visualization - bar chart, line chart, pie chart radar chart, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/data/charts/',
+  'image' => '',
+  'video' => '',
+);
