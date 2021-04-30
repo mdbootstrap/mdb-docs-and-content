774df45b1be3b36f3a227ac61e93734fe9f0d369
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-04-30 11:19:11 +0200

    Create order details extended documentation page in progress

--- en/_mdb5/standard/web/docs/extended/order-details/data.php
+++ en/_mdb5/standard/web/docs/extended/order-details/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[Name]',
-  'subheading' => 'Bootstrap 5 [Name] component',
-  'seo_title' => 'Bootstrap [Name] - examples & tutorial',
-  'description' => 'Responsive [Name] built with the latest Bootstrap 5. ',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended//',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/pro/_main/assets/mdb5-about-v2.jpg',
+  'main_title' => 'Order details',
+  'subheading' => 'Bootstrap 5 Order details component',
+  'seo_title' => 'Bootstrap Order details - examples & tutorial',
+  'description' => 'Responsive Order details built with the latest Bootstrap 5. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/order-details/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/order-details/assets/featured.png',
   'video' => '',
 );
