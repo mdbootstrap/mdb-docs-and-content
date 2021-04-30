8a3dd013e69c2835aa2aef124098ca83e977e1c1
ascensus <szymanskimichal9@gmail.com>
2021-04-30 11:59:36 +0200

    Update shopping cart docs meta

--- en/_mdb5/standard/web/docs/extended/shopping-carts/data.php
+++ en/_mdb5/standard/web/docs/extended/shopping-carts/data.php
@@ -2,9 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Shopping carts',
-  'subheading' => 'Bootstrap 5 Shopping carts component',
-  'seo_title' => 'Bootstrap Shopping carts - examples & tutorial',
-  'description' => 'Responsive Shopping carts built with the latest Bootstrap 5. ',
+  'subheading' => 'Bootstrap 5 Shopping Carts component',
+  'seo_title' => 'Bootstrap Shopping Carts - examples & templates',
+  'description' => 'Responsive Shopping Carts built with Bootstrap 5. Multiple examples of various designs and functionalities. Ready to use pages and templates.',
   'url' => 'https://mdbootstrap.com/docs/standard/extended/shopping-carts/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/shopping-carts/assets/featured.png',
   'video' => '',
