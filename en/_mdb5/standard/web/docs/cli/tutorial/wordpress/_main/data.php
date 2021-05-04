2e41404adfc47a98d0e63ce99327fd548edc54e5
ascensus <szymanskimichal9@gmail.com>
2021-05-04 07:21:53 +0200

    Update wordpress tutorial meta

--- en/_mdb5/standard/web/docs/cli/tutorial/wordpress/_main/data.php
+++ en/_mdb5/standard/web/docs/cli/tutorial/wordpress/_main/data.php
@@ -1,9 +1,9 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'WordPress Theme Development Tutorial',
-  'seo_title' => 'WordPress Theme Development Tutorial',
-  'description' => 'The most comprehensive tutorial about WordPress Theme Development with Bootstrap and Material Design',
+  'main_title' => 'WordPress & Bootstrap 5 Theme Development Tutorial',
+  'seo_title' => 'WordPress & Bootstrap 5 Theme Development Tutorial',
+  'description' => 'The most comprehensive tutorial about WordPress Theme Development with the newest Bootstrap 5 and Material Design',
   'url' => 'https://mdbootstrap.com/docs/standard/cli/tutorial/wordpress/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/cli/tutorial/wordpress/assets/00.jpg',
   'video' => 'https://www.youtube.com/watch?v=FFrJDBx7KfM&list=PLl1gkwYU90QlAGkN9Tt2YrHVqMdj20VkB&ab_channel=Keepcoding',
