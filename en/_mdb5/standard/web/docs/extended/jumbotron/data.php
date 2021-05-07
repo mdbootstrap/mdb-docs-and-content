76b7d3e9329a0ef5edde62c48e00ee3a4a42fe01
ascensus <szymanskimichal9@gmail.com>
2021-05-07 15:17:01 +0200

    Add jumbotron docs

---
+++ en/_mdb5/standard/web/docs/extended/jumbotron/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Jumbotron',
+  'subheading' => 'Bootstrap 5 Jumbotron component',
+  'seo_title' => 'Bootstrap Jumbotron - examples & tutorial',
+  'description' => 'Responsive Jumbotron built with Bootstrap 5. Examples of classic hero component, with background image, with navbar and many other combinations.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/jumbotron/',
+  'image' => 'https://mdbootstrap.com/wp-content/uploads/2017/08/jumbotron.jpg',
+  'video' => '',
+);
