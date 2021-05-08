c09cbe2cf3fa33770c45fd48c6ec36099931f808
ascensus <szymanskimichal9@gmail.com>
2021-05-08 14:19:22 +0200

    Add avatars docs

---
+++ en/_mdb5/standard/web/docs/extended/avatar/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Avatar',
+  'subheading' => 'Bootstrap 5 Avatar component',
+  'seo_title' => 'Bootstrap Avatar - examples & tutorial',
+  'description' => 'Responsive Avatar built with Bootstrap 5. Various templates like a circle avatar, square, inside the navbar, carousel, as a testimonial, profile and many more. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/avatar/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/extended/testimonials/assets/featured.jpg',
+  'video' => '',
+);
