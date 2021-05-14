d7f17039e8df8123f976e027354ee8e6dad2f411
Marcin Luczak <mbukacz@gmail.com>
2021-05-14 11:42:18 +0200

    Add MDB5 Vue sticky and touch docs

---
+++ en/_mdb5/vue/web/docs/directives/sticky/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Sticky',
+  'subheading' => 'Bootstrap 5 Sticky',
+  'seo_title' => 'Vue Bootstrap Sticky - examples & tutorial',
+  'description' => 'Sticky method for the latest Bootstrap 5 and Vue 3. Lock an element "glued" to the top or bottom of the viewport. You can also pin it to any other element.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/sticky/',
+  'image' => '',
+  'video' => '',
+);
---
+++ en/_mdb5/vue/web/docs/directives/touch/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Touch',
+  'subheading' => 'Bootstrap 5 Touch',
+  'seo_title' => 'Vue Bootstrap Touch - examples & tutorial',
+  'description' => 'Touch for the latest Bootstrap 5 and Vue 3. Custom events on touchscreen devices such as pinch, swipe, tap, press, pan and rotate.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/directives/touch/',
+  'image' => '',
+  'video' => '',
+);
