37a473617a1874086ccbaecc15d6050195040cfe
ascensus <szymanskimichal9@gmail.com>
2021-05-18 10:34:40 +0200

    Update drawer link

--- en/_mdb5/standard/web/docs/extended/drawer/data.php
+++ en/_mdb5/standard/web/docs/extended/drawer/data.php
@@ -5,7 +5,7 @@ $data_var = array(
   'subheading' => 'Bootstrap 5 Drawer component',
   'seo_title' => 'Bootstrap Drawer - examples & tutorial',
   'description' => 'Responsive Drawer built with the latest Bootstrap 5. Drawer allows you to display additional content within the boundaries of a given component (for example navigation links), by default invisible and displayed after clicking the trigger.',
-  'url' => 'https://mdbootstrap.com/docs/standard/navigation/drawer/',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/drawer/',
   'image' => 'https://mdbootstrap.com/wp-content/uploads/2015/08/sidenav-min.jpg',
   'video' => 'https://youtu.be/yFfkA9-0YIQ',
 );
