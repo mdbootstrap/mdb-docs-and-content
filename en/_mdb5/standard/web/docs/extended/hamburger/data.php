1b0318d4ae9bfe535c88c923b430c1627137c219
filipkappa <f.kapusta@mdbootstrap.com>
2021-05-19 14:09:32 +0200

    fix metadata

--- en/_mdb5/standard/web/docs/extended/hamburger/data.php
+++ en/_mdb5/standard/web/docs/extended/hamburger/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Hamburger',
+  'main_title' => 'Hamburger Menu',
   'subheading' => 'Bootstrap 5 Hamburger Menu',
   'seo_title' => 'Bootstrap Hamburger - examples & tutorial',
   'description' => 'Responsive Hamburger menu with Bootstrap 5. Templates include hamburger menu on the left and on the right (RTL support), icon animations, sidenav, navbar & more.',
-  'url' => 'https://mdbootstrap.com/docs/standard/extended/hamburger/',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/hamburger-menu/',
   'image' => 'https://mdbcdn.b-cdn.net/wp-content/uploads/2015/08/navbar-fb.jpg',
   'video' => '',
 );
