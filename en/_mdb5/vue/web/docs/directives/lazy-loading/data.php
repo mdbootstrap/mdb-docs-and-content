98ce2ccfee750af5345a5b468b285fc7e29cf03c
Marcin Luczak <mbukacz@gmail.com>
2021-05-27 14:54:30 +0200

    Add MDB5 Vue Lazy loading docs

---
+++ en/_mdb5/vue/web/docs/directives/lazy-loading/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Lazy loading',
+  'subheading' => 'Vue Bootstrap 5 Lazy loading',
+  'seo_title' => 'Vue Lazy loading - examples & tutorial',
+  'description' => 'Lazy loading for the latest Bootstrap 5 and Vue 3. Load pictures, videos, text, and other types of content on scroll. etc. A great solution for large-size multimedia files.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/lazy-loading/',
+  'image' => '',
+  'video' => '',
+);
