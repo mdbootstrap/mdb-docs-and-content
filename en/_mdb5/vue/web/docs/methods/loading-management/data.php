2617057a5ea1f92469a2e493ac8a016c711fbc54
Marcin Luczak <mbukacz@gmail.com>
2021-05-27 15:02:55 +0200

    Add MDB5 Vue Loading management docs

---
+++ en/_mdb5/vue/web/docs/methods/loading-management/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Loading management',
+  'subheading' => 'Vue Loading management',
+  'seo_title' => 'Vue Loading management - examples & tutorial',
+  'description' => 'Responsive loader for the latest Bootstrap 5 and Vue 3. Loading management helps to display an animation while loading data.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/loading-management/',
+  'image' => '',
+  'video' => '',
+);
