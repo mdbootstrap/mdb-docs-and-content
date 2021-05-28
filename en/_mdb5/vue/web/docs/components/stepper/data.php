80600ddbbc2d4999b749cebff72a12ad1f45e1cc
Marcin Luczak <mbukacz@gmail.com>
2021-05-28 13:16:55 +0200

    Add MDB5 Vue Stepper docs

---
+++ en/_mdb5/vue/web/docs/components/stepper/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Stepper',
+  'subheading' => 'Vue Stepper / Wizard component',
+  'seo_title' => 'Vue Stepper / Wizard - examples & tutorial',
+  'description' => 'Responsive stepper built with Bootstrap 5 and Vue 3. Form wizard, vertical stepper, multi step form validation, optional step, mobile stepper & more',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/components/stepper/',
+  'image' => '',
+  'video' => '',
+);
