a1167a379b602528ad26410f5923b1c7b156b883
Grzegorz Bujański <g.bujanski@mdbootstrap.com>
2021-06-01 13:08:34 +0200

    add rating docs

---
+++ en/_mdb5/angular/web/docs/components/rating/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Rating',
+  'subheading' => 'Angular Bootstrap 5 Star Rating component',
+  'seo_title' => 'Angular Bootstrap Star Rating - examples & tutorial',
+  'description' => 'Angular Responsive star rating built with the latest Bootstrap 5. Rating provides insight into others opinions and experiences with a product. Use stars or other custom symbols (i.e. smiling faces)',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/components/rating/',
+  'image' => '',
+  'video' => '',
+);
