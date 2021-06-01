d7d608cb5a34d59f5596381b807b10222fcb97fc
filipkappa <f.kapusta@mdbootstrap.com>
2021-06-01 15:55:23 +0200

    add faq extended page

---
+++ en/_mdb5/standard/web/docs/extended/faq/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'FAQ component / section',
+  'subheading' => 'Bootstrap 5 FAQ',
+  'seo_title' => 'Bootstrap FAQ component - examples & tutorial',
+  'description' => 'Responsive FAQ with Bootstrap 5. Templates include accordion faq, collapse FAQ, FAQ section & customized Bootstrap FAQ components.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/faq/',
+  'image' => 'https://mdbcdn.b-cdn.net/wp-content/uploads/2017/08/accordion-fb.jpg',
+  'video' => '',
+);
