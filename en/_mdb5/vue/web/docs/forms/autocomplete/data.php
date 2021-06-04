23dd5a0eaf37e389d46d8e3d6800b0ab9e324c48
Marcin Luczak <mbukacz@gmail.com>
2021-06-04 15:38:06 +0200

    Add MDB5 Vue infinite-scroll and autocomplete docs

---
+++ en/_mdb5/vue/web/docs/forms/autocomplete/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Autocomplete',
+  'subheading' => 'Vue Bootstrap 5 Autocomplete component',
+  'seo_title' => 'Vue Autocomplete - examples & tutorial',
+  'description' => 'Autocomplete built with the latest Bootstrap 5 and Vue 3. Automatically suggest option while click on form to faster complete search',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/forms/autocomplete/',
+  'image' => '',
+  'video' => '',
+);
