f0fc13615772c7c62c6cc4319a01cbf259baac02
Marcin Luczak <mbukacz@gmail.com>
2021-06-04 15:44:25 +0200

    Fix url typo

--- en/_mdb5/vue/web/docs/directives/infinite-scroll/data.php
+++ en/_mdb5/vue/web/docs/directives/infinite-scroll/data.php
@@ -1,11 +1,11 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Vue Lazy loading',
-  'subheading' => 'Vue Lazy loading',
-  'seo_title' => 'Vue Lazy loading - examples & tutorial',
-  'description' => 'Lazy loading for the latest Bootstrap 5 and Vue 3. Load pictures, videos, text, and other types of content on scroll. etc. A great solution for large-size multimedia files.',
-  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/lazy-loading/',
+  'main_title' => 'Infinite scroll',
+  'subheading' => 'Vue Bootstrap 5 Infinite scroll',
+  'seo_title' => 'Vue Infinite scroll - examples & tutorial',
+  'description' => 'Infinite scroll for the latest Bootstrap 5 and Vue 3. JS plugin that loads more content when scroll position nears the bottom of the page.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/infinite-scroll/',
   'image' => '',
   'video' => '',
 );
