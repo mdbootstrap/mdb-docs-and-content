8466380f9b3b15b650055d4f9883a78eb3f7fe4b
ascensus <szymanskimichal9@gmail.com>
2021-06-08 09:23:00 +0200

    Add filter docs

--- en/_mdb5/standard/web/docs/design-blocks/ecommerce/filters-and-sorting/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/filters-and-sorting/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'eCommerce blocks - categories',
-  'seo_title' => 'Bootstrap eCommerce blocks - categories',
-  'description' => 'Collection of practical examples of eCommerce categories. These kinds of sections are necessary when creating homepages or listing pages. You will find here solutions for all the possible use cases that can occur during creating eCommerce projects.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/categories/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/categories/assets/7.jpg',
+  'main_title' => 'eCommerce blocks - filters and sorting',
+  'seo_title' => 'Bootstrap eCommerce blocks - filters and sorting',
+  'description' => 'Collection of practical examples of eCommerce filters and sorting panels. These kinds of sections are necessary when creating shop pages with plenty of products. You will find here solutions for all the possible use cases that can occur during creating eCommerce projects.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/filters-and-sorting/',
+  'image' => 'https://mdbootstrap.com/img/screens/ecommerce/shop.png',
   'video' => 'https://youtu.be/gWs4y2DZngg',
 );
