a23a61aec806fe0165e46e0d7a01333df90efa38
ascensus <szymanskimichal9@gmail.com>
2021-06-08 12:27:03 +0200

    Add product docs

--- en/_mdb5/standard/web/docs/design-blocks/ecommerce/product/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/product/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'eCommerce blocks - categories',
-  'seo_title' => 'Bootstrap eCommerce blocks - categories',
-  'description' => 'Collection of practical examples of eCommerce categories. These kinds of sections are necessary when creating homepages or listing pages. You will find here solutions for all the possible use cases that can occur during creating eCommerce projects.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/categories/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/categories/assets/7.jpg',
+  'main_title' => 'eCommerce blocks - product details',
+  'seo_title' => 'Bootstrap eCommerce blocks - product details',
+  'description' => 'Collection of practical examples of eCommerce product blocks. These kinds of sections are necessary for every product page. ',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/product/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/product/assets/product.jpg',
   'video' => 'https://youtu.be/gWs4y2DZngg',
 );
