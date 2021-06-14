8c5fc60e5ad6a4c58e14925f736c13667d946772
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-06-14 09:10:53 +0200

    add mdb5 react lightbox docs

---
+++ en/_mdb5/react/web/docs/components/lightbox/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Lightbox',
+  'subheading' => 'React',
+  'seo_title' => 'React Lightbox - examples & tutorial',
+  'description' => 'Responsive lightbox built with the latest Bootstrap 5. Lightbox is a responsive gallery with the option to enlarge selected photos.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/lightbox/',
+  'image' => '',
+  'video' => '',
+);
