e6a89672eeceaf83740749cceb7b4893330ae564
Marcin Luczak <mbukacz@gmail.com>
2021-06-14 10:31:38 +0200

    Add MDB5 Vue Smooth-Scroll, Popconfirm and Lightbox docs

---
+++ en/_mdb5/vue/web/docs/directives/smooth-scroll/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Smooth scroll',
+  'subheading' => 'Vue Smooth scroll',
+  'seo_title' => 'Vue Bootstrap Smooth scroll - examples & tutorial',
+  'description' => 'Smooth scroll for the latest Bootstrap 5 and Vue 3. Slide to a section of a page with an animated scrolling effect. Trigger via data attribute or other options.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/methods/smooth-scroll/',
+  'image' => '',
+  'video' => '',
+);
