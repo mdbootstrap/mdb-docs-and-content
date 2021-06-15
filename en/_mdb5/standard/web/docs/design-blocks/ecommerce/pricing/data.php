bc1e9a81c5b7e158eee6ca576817643e71d2b5ca
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-06-15 20:12:46 +0200

    Create ecommerce pricing docs page in progress

--- en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'eCommerce blocks - product list & grid',
-  'seo_title' => 'Bootstrap eCommerce blocks - product list & grid',
-  'description' => 'Collection of practical examples of product lists and product grids. These kinds of sections are necessary when creating listing pages and shop filters. You will find here solutions for all the possible use cases that can occur during creating eCommerce projects.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/product-lists-and-grids/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/product-lists-and-grids/assets/2.jpg',
-  'video' => 'https://youtu.be/gWs4y2DZngg',
+  'main_title' => 'eCommerce blocks - pricing',
+  'seo_title' => 'Bootstrap eCommerce blocks - picing',
+  'description' => 'Collection of practical examples of pricing.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/pricing/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/assets/1.jpg',
+  'video' => '',
 );
