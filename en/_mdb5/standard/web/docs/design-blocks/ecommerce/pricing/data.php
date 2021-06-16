a9beb6eef221522de11948bd22185345388e0012
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-06-16 20:36:01 +0200

    Create landing page newsletter design blocks docs page

--- en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'eCommerce blocks - pricing',
-  'seo_title' => 'Bootstrap eCommerce blocks - picing',
+  'seo_title' => 'Bootstrap eCommerce blocks - pricing',
   'description' => 'Collection of practical examples of pricing.',
   'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/pricing/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/assets/1.jpg',
--- en/_mdb5/standard/web/docs/design-blocks/landing-page/newsletter/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/newsletter/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'eCommerce blocks - pricing',
-  'seo_title' => 'Bootstrap eCommerce blocks - picing',
-  'description' => 'Collection of practical examples of pricing.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/pricing/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/pricing/assets/1.jpg',
+  'main_title' => 'Landing Page blocks - newsletter',
+  'seo_title' => 'Bootstrap Landing Page blocks - newsletter',
+  'description' => 'Collection of practical examples of newsletter.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/newsletter/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/newsletter/assets/4.jpg',
   'video' => '',
 );
