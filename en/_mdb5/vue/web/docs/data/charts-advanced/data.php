f77bcfa1524e1286bd28d6ca3d03a638b461a8dc
Marcin Luczak <mbukacz@gmail.com>
2021-06-21 12:01:05 +0200

    Add MDB5 Vue Accordion andCharts advanced docs

---
+++ en/_mdb5/vue/web/docs/data/charts-advanced/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Charts advanced usage',
+  'subheading' => 'Vue Bootstrap 5 Charts advanced usage',
+  'seo_title' => 'Vue Bootstrap Charts advanced usage - examples & tutorial',
+  'description' => 'Advanced Charts built with the latest Bootstrap 5 and Vue 3. Easy to customize advanced options for bar, line, pie and radar charts.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/data/charts-advanced/',
+  'image' => '',
+  'video' => '',
+);
