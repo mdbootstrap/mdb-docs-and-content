e0e695292d81e71986e1dec4ed0dbb16cafc2af4
m.duszak <m.duszak@mdbootstrap.com>
2021-06-23 16:53:12 +0200

    docs: focus trap

---
+++ en/_mdb5/standard/web/docs/methods/focus-trap/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Focus trap',
+  'subheading' => 'Bootstrap 5 Focus trap',
+  'seo_title' => 'Bootstrap Focus trap - examples & tutorial',
+  'description' => 'Infinite scroll for the latest Bootstrap 5. JS utility that allows you to trap focus on destined elements.',
+  'url' => 'https://mdbootstrap.com/docs/standard/methods/focus-trap/',
+  'image' => '',
+  'video' => '',
+);
