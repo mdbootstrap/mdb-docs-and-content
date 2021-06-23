5e513172e33b74e85eefae3bd477f742f40b51e4
Mateusz Leciejewski <m.leciejewski@mdbootstrap.com>
2021-06-23 10:17:23 +0200

    add square buttons extended docs

---
+++ en/_mdb5/standard/web/docs/extended/square-buttons/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Square Buttons',
+  'subheading' => 'Bootstrap 5 Square Buttons component',
+  'seo_title' => 'Bootstrap Square Buttons - examples & tutorial',
+  'description' => 'Responsive Square Buttons built with Bootstrap 5. Black, disabled, full-width outline, social, block square buttons examples.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/square-buttons/',
+  'image' => 'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/components/buttons/buttons.jpg',
+  'video' => '',
+);
