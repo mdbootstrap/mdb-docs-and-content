6691d6de1ed0eadb6523d462903c92fb8d5d6448
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-06-23 15:17:57 +0200

    Update landing page design blocks headers docs page

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/headers/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/headers/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Landing Page blocks - hero sections',
-  'seo_title' => 'Bootstrap Landing Page blocks - hero sections',
-  'description' => 'Collection of practical examples of hero sections.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/hero-sections/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/hero-sections/assets/1.jpg',
+  'main_title' => 'Landing Page blocks - headers',
+  'seo_title' => 'Bootstrap Landing Page blocks - headers',
+  'description' => 'Collection of practical examples of headers.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/headers/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/headers/assets/5.jpg',
   'video' => '',
 );
