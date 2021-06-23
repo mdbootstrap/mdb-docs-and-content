b27d5e1ffa96acd32630324c43b32923d1514f42
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-06-23 15:41:29 +0200

    Update landing page design blocks news docs page

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/news/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/news/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Landing Page blocks - stats',
-  'seo_title' => 'Bootstrap Landing Page blocks - stats',
-  'description' => 'Collection of practical examples of stats.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/stats/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/stats/assets/3.jpg',
+  'main_title' => 'Landing Page blocks - news',
+  'seo_title' => 'Bootstrap Landing Page blocks - news',
+  'description' => 'Collection of practical examples of news.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/news/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/news/assets/2.jpg',
   'video' => '',
 );
