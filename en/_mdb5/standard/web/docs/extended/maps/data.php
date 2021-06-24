374786a4841ebd6847fdffd301f7e71afbbcd7a1
filipkappa <f.kapusta@mdbootstrap.com>
2021-06-24 11:14:26 +0200

    add map docs

---
+++ en/_mdb5/standard/web/docs/extended/maps/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Maps',
+  'subheading' => 'Bootstrap 5 Maps',
+  'seo_title' => 'Bootstrap maps - examples & tutorial',
+  'description' => 'Responsive maps with Bootstrap 5. Google maps, vector maps, contact us section template & more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/maps/',
+  'image' => 'https://mdbcdn.b-cdn.net/wp-content/uploads/2017/09/map-fb.jpg',
+  'video' => '',
+);
