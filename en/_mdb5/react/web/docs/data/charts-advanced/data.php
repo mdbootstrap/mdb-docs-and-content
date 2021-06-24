6de3cfb2c6fc48b344449edb279125bde2991d15
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-06-24 13:57:46 +0200

    add charts advanced and update lightbox and select mdb5 react docs

---
+++ en/_mdb5/react/web/docs/data/charts-advanced/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Charts advanced usage',
+  'subheading' => 'Bootstrap 5 Charts advanced usage',
+  'seo_title' => 'React Charts advanced usage - examples & tutorial',
+  'description' => 'Advanced Charts built with the latest Bootstrap 5. Easy to customize advanced options for bar, line, pie and radar charts.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/data/charts-advanced/',
+  'image' => '',
+  'video' => '',
+);
