46e204b91c0d3d6698afd9fb03b8855e110352c1
Pan Seba <sebos.k10@gmail.com>
2021-06-24 09:01:56 +0200

    Fix typo

--- en/_mdb5/angular/web/docs/content-styles/colors/data.php
+++ en/_mdb5/angular/web/docs/content-styles/colors/data.php
@@ -2,7 +2,7 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Colors',
-  'subheading' => 'Angular Bootstrap 5 Colors',s
+  'subheading' => 'Angular Bootstrap 5 Colors',
   'seo_title' => 'Angular Colors with Bootstrap - examples & tutorial',
   'description' => 'Colors built with Bootstrap 5, Angular 11 and Material Design 2.0.Enables comprehensive color customization of theme, background, text, links, buttons and components.',
   'url' => 'https://mdbootstrap.com/docs/b5/angular/content-styles/colors/',
