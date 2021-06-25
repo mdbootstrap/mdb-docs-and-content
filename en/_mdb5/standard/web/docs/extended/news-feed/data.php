21a37c79b667e622030225e2475767b1cfac17d3
Mateusz Leciejewski <m.leciejewski@mdbootstrap.com>
2021-06-25 10:14:15 +0200

    add news feed extended docs

---
+++ en/_mdb5/standard/web/docs/extended/news-feed/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'News feed',
+  'subheading' => 'Bootstrap 5 News feed component',
+  'seo_title' => 'Bootstrap News feed - examples & tutorial',
+  'description' => 'Responsive News Feed built with the Bootstrap 5. Enhance your project with a variety of social sections such as news feed, comments, and post cards.',
+  'url' => 'https://mdbootstrap.com/docs/standard/extended/news-feed/',
+  'image' => 'https://mdbcdn.b-cdn.net/wp-content/uploads/2017/12/social-fb.jpg',
+  'video' => '',
+);
