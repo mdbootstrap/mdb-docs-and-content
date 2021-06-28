77a967038d6e5ed65da50124ef22b126a4f2f804
idzark <a.idzikowski91@gmail.com>
2021-06-28 11:53:54 +0200

    Add A5 autocomplete docs

---
+++ en/_mdb5/angular/web/docs/forms/autocomplete/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Autocomplete',
+  'subheading' => 'Angular Bootstrap 5 Autocomplete component',
+  'seo_title' => 'Angular Bootstrap Autocomplete - examples & tutorial',
+  'description' => 'Angular Autocomplete built with the latest Bootstrap 5. Automatically suggest option while click on form to faster complete search',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/forms/autocomplete/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
