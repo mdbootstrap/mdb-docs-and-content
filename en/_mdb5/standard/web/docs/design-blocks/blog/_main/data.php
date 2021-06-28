99373a16febb01d4e4bcd92e40ae8d19150c5354
ascensus <szymanskimichal9@gmail.com>
2021-06-28 12:17:21 +0200

    Add post assets

---
+++ en/_mdb5/standard/web/docs/design-blocks/blog/_main/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'eCommerce blocks',
+  'seo_title' => 'eCommerce blocks & product grid - examples & design blocks',
+  'description' => 'Responsive eCommerce design blocks & product grids built with Bootstrap 5. Built your own online store using ready-made sections such as lists, products, store home page, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/',
+  'image' => '',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
