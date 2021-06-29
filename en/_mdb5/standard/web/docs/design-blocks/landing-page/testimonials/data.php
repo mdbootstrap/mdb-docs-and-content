408cfa5a9e5f578561d1b060950998f82644a635
ascensus <szymanskimichal9@gmail.com>
2021-06-29 13:02:28 +0200

    Add testimonial content

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/testimonials/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/testimonials/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[name]',
-  'seo_title' => '[name]',
-  'description' => 'Collection of practical examples of Landing Page feature sections.',
+  'main_title' => 'Landing Page testominial sections',
+  'seo_title' => 'Landing Page testominial sections - Bootstrap 5 & Material Design',
+  'description' => 'Collection of practical examples of Landing Page testominial sections.',
   'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/testimonials/',
-  'image' => '',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/testimonials/assets/6.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
