936e93f93952561b6b840cc88ead9ac6b789353b
ascensus <szymanskimichal9@gmail.com>
2021-06-29 12:17:02 +0200

    Add FAQ content

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/faq/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/faq/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[name]',
-  'seo_title' => '[name]',
-  'description' => 'Collection of practical examples of Landing Page feature sections.',
-  'url' => '',
-  'image' => '',
+  'main_title' => 'FAQ section',
+  'seo_title' => 'Frequently Asked Questions - Bootstrap 5 design blocks',
+  'description' => 'Collection of practical examples of Landing Page FAQ sections.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/faq/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/faq/assets/3.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
