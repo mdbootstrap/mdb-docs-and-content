23d58d2d21e5bd2dde20558594a2d81ca40c981b
ascensus <szymanskimichal9@gmail.com>
2021-06-29 12:48:37 +0200

    Add team content

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/testimonials/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/testimonials/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => '[name]',
   'seo_title' => '[name]',
   'description' => 'Collection of practical examples of Landing Page feature sections.',
-  'url' => '',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/testimonials/',
   'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
