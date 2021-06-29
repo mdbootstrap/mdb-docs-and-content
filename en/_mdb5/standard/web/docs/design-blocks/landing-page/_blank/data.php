0ff511631e6f258789007eaae2a97a33f6c32639
ascensus <szymanskimichal9@gmail.com>
2021-06-29 11:51:07 +0200

    Update landing page blocks

--- en/_mdb5/standard/web/docs/design-blocks/landing-page/_blank/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/_blank/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Landing Page Call To Action sections',
-  'seo_title' => 'Landing Page Call To Action sections - Bootstrap 5 & Material Design',
-  'description' => 'Collection of practical examples of Landing Page CTA sections.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/cta/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/cta/assets/5.jpg',
+  'main_title' => '[name]',
+  'seo_title' => '[name]',
+  'description' => 'Collection of practical examples of Landing Page feature sections.',
+  'url' => '',
+  'image' => '',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
