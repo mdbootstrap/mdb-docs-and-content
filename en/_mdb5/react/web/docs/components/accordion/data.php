d83e493ae52cb7982c30203e3c88b2f595773aa4
Wojstan <wpstaniszewski@gmail.com>
2021-06-29 11:50:21 +0200

    add accordion docs fix

--- en/_mdb5/react/web/docs/components/accordion/data.php
+++ en/_mdb5/react/web/docs/components/accordion/data.php
@@ -3,7 +3,7 @@ global $data_var;
 $data_var = array(
   'main_title' => 'Accordion',
   'subheading' => 'React Bootstrap 5 Accordion component',
-  'seo_title' => 'Raect Accordion - examples & tutorial',
+  'seo_title' => 'React Accordion - examples & tutorial',
   'description' => 'Responsive accordion built with the latest Bootstrap 5. Accordion is a vertically collapsing element to show and hide content via class changes.  ',
   'url' => 'https://mdbootstrap.com/docs/b5/react/components/accordion/',
   'image' => '',
