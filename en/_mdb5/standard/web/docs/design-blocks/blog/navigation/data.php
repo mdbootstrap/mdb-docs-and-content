1005b2e779dd12ced4be0886fa53629b96f30932
ascensus <szymanskimichal9@gmail.com>
2021-06-29 08:09:21 +0200

    Add blog home page assets

--- en/_mdb5/standard/web/docs/design-blocks/blog/navigation/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/blog/navigation/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Blog blocks - post',
   'seo_title' => 'Bootstrap Blog blocks - post',
   'description' => 'Collection of practical examples of blog post elements. These kinds of sections are necessary when creating articles. You will find here solutions for all the possible use cases that can occur during creating Blog projects.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/blog/post/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/blog/post/assets/1.jpg',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/Blog/post/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/blog/post/assets/1.webp',
   'video' => 'https://youtu.be/gWs4y2DZngg',
 );
