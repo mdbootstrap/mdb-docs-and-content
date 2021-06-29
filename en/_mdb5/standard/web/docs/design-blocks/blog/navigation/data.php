976fd2c9148d9d4ca61bd186a88837a5f678fc81
ascensus <szymanskimichal9@gmail.com>
2021-06-29 09:07:39 +0200

    Add navigation blog content

--- en/_mdb5/standard/web/docs/design-blocks/blog/navigation/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/blog/navigation/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Blog blocks - post',
-  'seo_title' => 'Bootstrap Blog blocks - post',
-  'description' => 'Collection of practical examples of blog post elements. These kinds of sections are necessary when creating articles. You will find here solutions for all the possible use cases that can occur during creating Blog projects.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/Blog/post/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/blog/post/assets/1.webp',
+  'main_title' => 'Blog blocks - navigation',
+  'seo_title' => 'Bootstrap Blog blocks - navigation',
+  'description' => 'Collection of practical examples of blog navigation elements. These kinds of sections are necessary when creating any blogs. You will find here solutions for all the possible use cases that can occur during creating Blog projects.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/blog/navigation/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/blog/navigation/assets/4.jpg',
   'video' => 'https://youtu.be/gWs4y2DZngg',
 );
