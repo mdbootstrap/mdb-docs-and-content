ca5da98c57a227a3432585ccda7332eddc5e6d71
idzark <a.idzikowski91@gmail.com>
2021-07-07 16:00:59 +0200

    Rename mdb-select-option to mdb-option

---
+++ en/_mdb5/angular/web/docs/methods/loading-management/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Loading management',
+  'subheading' => 'Bootstrap 5 Loading management',
+  'seo_title' => 'Bootstrap Loading management - examples & tutorial',
+  'description' => 'Responsive loader for the latest Bootstrap 5. Loading management helps to display an animation while loading data.',
+  'url' => 'https://mdbootstrap.com/docs/standard/methods/loading-management/',
+  'image' => '',
+  'video' => '',
+);
