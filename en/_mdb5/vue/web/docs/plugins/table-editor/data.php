b7eb5cf253a6bd6794b979354b821449404a7818
Marcin Luczak <mbukacz@gmail.com>
2021-07-18 16:04:21 +0200

    Add MDB5 Vue Table Editor docs

---
+++ en/_mdb5/vue/web/docs/plugins/table-editor/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Table editor',
+  'subheading' => 'Vue Bootstrap 5 Table editor plugin',
+  'seo_title' => 'Vue Table editor - examples & tutorial',
+  'description' => 'Responsive interactive built with the latest Bootstrap 5 and Vue 3. Creates editable tables. Delete or edit rows directly or via modal editor.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/table-editor/',
+  'image' => '',
+  'video' => '',
+);
