555ef4614cab6c6e7b744fff0aefd5a174fcfb6c
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-07-22 14:16:37 +0200

    add mdb5 reacct theme docs

---
+++ en/_mdb5/react/web/docs/content-styles/theme/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Theme',
+  'subheading' => 'React Bootstrap 5 Theme',
+  'seo_title' => 'React 5 Theme - examples & tutorial',
+  'description' => 'Appearance customization options enable you to create skins for your Bootstrap 5 project. Use predefined dark theme, light theme or create custom themes.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/content-styles/theme/',
+  'image' => '',
+  'video' => '',
+);
