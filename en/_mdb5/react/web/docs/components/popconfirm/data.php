6fc1b335f4d8a7cbe111012b19143b93cca9d7c7
Wojciech Staniszewski <w.staniszewski@mdbootstrap.com>
2021-07-27 14:27:43 +0200

    add scrollspy fixes

--- en/_mdb5/react/web/docs/components/popconfirm/data.php
+++ en/_mdb5/react/web/docs/components/popconfirm/data.php
@@ -5,7 +5,7 @@ $data_var = array(
   'subheading' => 'React Bootstrap 5 Popconfirm component',
   'seo_title' => 'React Popconfirm - examples & tutorial',
   'description' => 'Responsive popconfirm built with the latest Bootstrap 5. Popconfirm is a compact dialog alert.  Use it to confirm/cancel a decision or display extra content like an explanation.',
-  'url' => 'https://mdbootstrap.com/docs/b5/react/methods/popconfirm/',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/components/popconfirm/',
   'image' => '',
   'video' => '',
 );
