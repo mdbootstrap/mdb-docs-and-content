288850eda72e888c3974a059cd6278c201b5a908
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-02 11:33:48 +0200

    Add admin navigation design blocks docs page

--- en/_mdb5/standard/web/docs/design-blocks/admin/navigation/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/admin/navigation/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[name]',
-  'seo_title' => '[name]',
-  'description' => 'Collection of practical examples of Admin design blocks.',
-  'url' => '',
-  'image' => '',
+  'main_title' => 'Admin blocks - navigation',
+  'seo_title' => 'Bootstrap Admin blocks - navigation',
+  'description' => 'Collection of practical examples of Admin navigation design blocks .',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/admin/navigation/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/admin/navigation/assets/1.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
