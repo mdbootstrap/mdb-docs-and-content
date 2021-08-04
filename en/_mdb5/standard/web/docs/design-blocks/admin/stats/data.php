b1c37a323ecce85646123dc39e3cee6bf86b0e6a
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-04 21:53:10 +0200

    Create new admin blocks stats

--- en/_mdb5/standard/web/docs/design-blocks/admin/stats/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/admin/stats/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[name]',
-  'seo_title' => '[name]',
-  'description' => 'Collection of practical examples of Admin design blocks.',
-  'url' => '',
-  'image' => '',
+  'main_title' => 'Admin blocks - stats',
+  'seo_title' => 'Bootstrap Admin blocks - stats',
+  'description' => 'Collection of practical examples of Admin stats design blocks.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/admin/stats/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/admin/stats/assets/1.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
