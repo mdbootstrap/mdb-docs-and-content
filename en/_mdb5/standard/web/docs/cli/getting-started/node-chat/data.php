41c5fbd0589aab91c5febbcd28b3c90a3b71413c
filipkappa <f.kapusta@mdbootstrap.com>
2021-08-05 08:04:17 +0200

    fix CLI metadata errors

--- en/_mdb5/standard/web/docs/cli/getting-started/node-chat/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/node-chat/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Node/SocketIO chat app',
   'seo_title' => 'Node/SocketIO chat app',
   'description' => 'Create, deploy and host your MDB project with a single command',
-  'url' => 'https://mdbootstrap.com/docs/standard/cli/about/',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/',
   'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
   'video' => 'https://www.youtube.com/watch?v=-pIJnQQMexg',
 );
