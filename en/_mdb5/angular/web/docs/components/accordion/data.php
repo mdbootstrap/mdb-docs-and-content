6fcef06c15206fce9eb82e7e4685650a69b5013c
idzark <a.idzikowski91@gmail.com>
2021-08-09 09:31:41 +0200

    Add new A5 pages and examples

---
+++ en/_mdb5/angular/web/docs/components/accordion/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Accordion',
+  'subheading' => 'Angular Bootstrap 5 Accordion component',
+  'seo_title' => 'Angular Bootstrap Accordion - examples & tutorial',
+  'description' => 'Responsive Angular accordion built with the latest Bootstrap 5. Accordion is a vertically collapsing element to show and hide content via class changes.  ',
+  'url' => 'https://mdbootstrap.com/docs/standard/components/accordion/',
+  'image' => '',
+  'video' => '',
+);
