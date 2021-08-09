d99b07fb843ae4f52227e4cd6a2cf8ed9e21c46e
Grzegorz <g.bujanski@mdbootstrap.com>
2021-08-09 10:47:30 +0200

    add angular lightbox docs

---
+++ en/_mdb5/angular/web/docs/components/lightbox/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Lightbox',
+  'subheading' => 'Angular Bootstrap 5 Lightbox component',
+  'seo_title' => 'Angular Bootstrap Lightbox - examples & tutorial',
+  'description' => 'Responsive lightbox built with ootstrap 5, Angular 11 and Material Design 2.0. Lightbox is a responsive gallery with the option to enlarge selected photos.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/components/lightbox/',
+  'image' => '',
+  'video' => '',
+);
