f08b1250fd0da5ea9a3739c06b2510bd0e90cff5
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-10 20:26:23 +0200

    Add new admin design blocks

--- en/_mdb5/standard/web/docs/design-blocks/admin/_main/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/admin/_main/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'Blog blocks',
-  'seo_title' => 'Blog blocks - Bootstrap 5 & Material Design 2.0 design blocks',
-  'description' => 'Responsive Blog blocks built with the latest Bootstrap 5 and stunning Material Design 2.0. Create your own Blog with various sections for the article, homepage, and more.',
-  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/blog/',
-  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/blog/home/assets/1.jpg',
+  'main_title' => 'Admin blocks',
+  'seo_title' => 'Admin blocks - Bootstrap 5 & Material Design 2.0 design blocks',
+  'description' => 'Responsive Admin blocks built with the latest Bootstrap 5 and stunning Material Design 2.0. Create your own Admin with various sections for the navigation, tables, charts and more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/admin/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/admin/charts/assets/3.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
