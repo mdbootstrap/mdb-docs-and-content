0638f80568ddd6b24822009ad8d0f8d628d8a5c4
unknown <Martussky>
2021-08-12 14:21:00 +0200

    Add design blocks admin complex docs page

--- en/_mdb5/standard/web/docs/design-blocks/admin/complex/data.php
+++ en/_mdb5/standard/web/docs/design-blocks/admin/complex/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => '[name]',
-  'seo_title' => '[name]',
-  'description' => 'Collection of practical examples of Admin design blocks.',
-  'url' => '',
-  'image' => '',
+  'main_title' => 'Admin blocks - complex',
+  'seo_title' => 'Bootstrap Admin blocks - complex',
+  'description' => 'Collection of practical examples of Admin complex design blocks.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/admin/complex/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/admin/complex/assets/1.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
 );
