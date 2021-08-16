a66b704efb2b6c3dce272db6ba1e8f45e5b1d979
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-16 16:41:14 +0200

    Add design blocks landing page banners docs page

---
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/banners/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Landing Page banners',
+  'seo_title' => 'Landing Page banners',
+  'description' => 'Collection of practical examples of Landing Page banners.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/banners/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/banners/assets/2.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
