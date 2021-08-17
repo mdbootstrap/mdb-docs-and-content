5e0223b3bad78d3f92cea670b63444d0da73fbcc
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-17 20:08:45 +0200

    Create landing page mega menu design blocks docs page

---
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/mega-menu/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Landing Page mega menu',
+  'seo_title' => 'Landing Page mega menu',
+  'description' => 'Collection of practical examples of Landing Page mega menu.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/mega-menu/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/mega-menu/assets/1.jpg',
+  'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
+);
