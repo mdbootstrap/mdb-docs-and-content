6fe72060bf0a9288be659c50fb12d68c09f310d6
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-08-24 20:55:14 +0200

    Add projects design blocks

---
+++ en/_mdb5/standard/web/docs/design-blocks/landing-page/project-sections/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Landing Page blocks - project sections',
+  'seo_title' => 'Bootstrap Landing Page blocks - project sections',
+  'description' => 'Collection of practical examples of project sections.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/landing-page/project-sections/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/landing-page/project-sections/assets/1.jpg',
+  'video' => '',
+);
