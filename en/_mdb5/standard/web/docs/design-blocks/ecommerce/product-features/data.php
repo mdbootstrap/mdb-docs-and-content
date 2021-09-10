42bd73f9a256b4db7080974735e069def4a6cc9d
Marta Szymanska <m.szymanska@mdbootstrap.com>
2021-09-10 10:29:27 +0200

    Add product features ecommerce design blocks docs page

---
+++ en/_mdb5/standard/web/docs/design-blocks/ecommerce/product-features/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'eCommerce blocks - product features',
+  'seo_title' => 'Bootstrap eCommerce blocks - product features',
+  'description' => 'Collection of practical examples of eCommerce product features blocks.',
+  'url' => 'https://mdbootstrap.com/docs/standard/design-blocks/ecommerce/product-features/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/design-blocks/ecommerce/product-features/assets/1.jpg',
+  'video' => 'https://youtu.be/gWs4y2DZngg',
+);
