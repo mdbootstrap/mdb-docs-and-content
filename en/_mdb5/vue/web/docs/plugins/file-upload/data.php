6a61d561d9fddd2dab102ebb417c283accb51cbf
Mikolaj Smolenski <smolenski.mikolaj@gmail.com>
2021-09-16 10:00:23 +0200

    Vue - add file upload plugin docs & fix some standard issues

---
+++ en/_mdb5/vue/web/docs/plugins/file-upload/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue File upload',
+  'subheading' => 'Vue Bootstrap 5 File upload plugin',
+  'seo_title' => 'Vue Bootstrap File upload - examples & tutorial',
+  'description' => 'File upload plugin built with the latest Bootstrap 5. Many customization options like custom height, max size, confirmation message, and much more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/file-upload/',
+  'image' => '',
+  'video' => '',
+);
