362c4f277a7eb8e8615e00e2822c0f892aff68eb
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-09-20 11:00:49 +0200

    add mdb5 react vector maps docs

---
+++ en/_mdb5/react/web/docs/plugins/vector-maps/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vector maps',
+  'subheading' => 'React Vector maps plugin',
+  'seo_title' => 'React Vector maps - examples & tutorial',
+  'description' => 'Responsive Vector maps plugin built with the latest Bootstrap 5. Customizable map display options such as , custom zoom, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/plugins/vector-maps/',
+  'image' => '',
+  'video' => '',
+);
