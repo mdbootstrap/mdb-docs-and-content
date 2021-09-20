f5945e04c079e534be81e25abee141adfa09ad1a
Grzegorz <g.bujanski@mdbootstrap.com>
2021-09-20 15:37:30 +0200

    add calendar & table-ediutor docs

---
+++ en/_mdb5/angular/web/docs/plugins/calendar/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Calendar',
+  'subheading' => 'Angular Bootstrap 5 Calendar plugin',
+  'seo_title' => 'Angular Bootstrap Calendar - examples & tutorial',
+  'description' => 'Responsive Calendar plugin built with the Bootstrap 5, Angular 11 and Material Design 2.0. Various customization options like default view, event formats, customizable captions, and much more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/plugins/calendar/',
+  'image' => '',
+  'video' => '',
+);
---
+++ en/_mdb5/angular/web/docs/plugins/table-editor/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Table editor',
+  'subheading' => 'Angular Bootstrap 5 Table editor plugin',
+  'seo_title' => 'Angular Bootstrap Table editor - examples & tutorial',
+  'description' => 'Responsive interactive built with the Bootstrap 5, Angular 11 and Material Design 2.0. Creates editable tables. Delete or edit rows directly or via modal editor.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/plugins/table-editor/',
+  'image' => '',
+  'video' => '',
+);
