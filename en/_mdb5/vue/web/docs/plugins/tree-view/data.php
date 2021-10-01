e41d677e2bb0be7f4cd74043ceb226d8a67cb3e3
Marcin Łuczak <m.luczak@mdbootstrap.com>
2021-10-01 14:21:02 +0200

    Add MDB5 Vue Treeview plugin docs

---
+++ en/_mdb5/vue/web/docs/plugins/tree-view/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Tree view' ,
+  'subheading' => 'Vue Bootstrap 5 Tree view plugin',
+  'seo_title' => 'Vue Bootstrap Tree view - examples & tutorial',
+  'description' => 'Tree view plugin built with the latest Bootstrap 5. Plenty of customization examples such as accordion, selectable, expand, color, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/tree-view/',
+  'image' => '',
+  'video' => '',
+);
