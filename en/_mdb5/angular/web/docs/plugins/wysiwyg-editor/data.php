403e34ae21560488f861f388e10d31e6ba51ae55
Grzegorz <g.bujanski@mdbootstrap.com>
2021-10-04 09:04:06 +0200

    add wysiwyg angular plugin docs

---
+++ en/_mdb5/angular/web/docs/plugins/wysiwyg-editor/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'WYSIWYG editor',
+  'subheading' => 'Angular Bootstrap 5 WYSIWYG editor plugin',
+  'seo_title' => 'Angular Bootstrap WYSIWYG editor - examples & tutorial',
+  'description' => 'Responsive WYSIWYG editor built with the latest Bootstrap 5, Angular 11 and Material Design 2.0. Plenty of customization examples such as custom colors, translations, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/standard/plugins/wysiwyg-editor/',
+  'image' => '',
+  'video' => '',
+);
