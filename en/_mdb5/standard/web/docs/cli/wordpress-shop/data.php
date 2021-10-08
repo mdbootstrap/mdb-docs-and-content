778a7d9b900210f2f87f6f5f4f2e696a9b3a5269
Adam Jakubowski <a.jakubowski@mdbootstrap.com>
2021-10-08 15:31:00 +0200

    Add wordpress themes blog and shop

---
+++ en/_mdb5/standard/web/docs/cli/wordpress-shop/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'MDB WordPress Shop Theme',
+  'seo_title' => 'MDB CLI WordPress Shop Theme',
+  'description' => 'Create, deploy and host your MDB project with a single command',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/',
+  'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
+  'video' => 'https://www.youtube.com/watch?v=-pIJnQQMexg',
+);
