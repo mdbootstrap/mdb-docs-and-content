163b84a99fcc0662bd4553fa524e04b8f1bc12d1
idzark <a.idzikowski91@gmail.com>
2021-10-18 13:28:11 +0200

    Add A5 dnd and vector maps docs

---
+++ en/_mdb5/angular/web/docs/plugins/vector-maps/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vector maps',
+  'subheading' => 'Angular Bootstrap 5 Vector maps plugin',
+  'seo_title' => 'Angular Bootstrap Vector maps - examples & tutorial',
+  'description' => 'Responsive Angular Vector maps plugin built with the latest Bootstrap 5. Customizable map display options such as , custom zoom, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/angular/plugins/vector-maps/',
+  'image' => '',
+  'video' => '',
+);
