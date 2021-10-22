43546f59f51cd3643e047235b91354ebbab6196b
Marcin Łuczak <m.luczak@mdbootstrap.com>
2021-10-22 15:39:41 +0200

    Add MDB5 Vue Onboarding docs

---
+++ en/_mdb5/vue/web/docs/plugins/onboarding/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Onboarding',
+  'subheading' => 'Vue Bootstrap 5 Onboarding plugin',
+  'seo_title' => 'Vue Bootstrap Onboarding - examples & tutorial',
+  'description' => 'Responsive Onboarding plugin built with the latest Bootstrap 5 and Vue 3. It is is a guide plugin to show users how to use your website.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/onboarding/',
+  'image' => '',
+  'video' => '',
+);
