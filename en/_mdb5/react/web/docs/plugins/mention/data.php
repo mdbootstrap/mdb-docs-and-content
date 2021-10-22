155b3c642b254a2f051317f28ce0792ea7e15d7f
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-10-22 15:32:07 +0200

    add mdb5 react mention docs

---
+++ en/_mdb5/react/web/docs/plugins/mention/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Mention',
+  'subheading' => 'React 5 Mention plugin',
+  'seo_title' => 'React Bootstrap Mention - examples & tutorial',
+  'description' => 'Responsive Mention plugin built with the latest Bootstrap 5.  Mention is an autocomplete dropdown menu for your search inputs. It is useful for tagging users or topics.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/plugins/mention/',
+  'image' => '',
+  'video' => '',
+);
