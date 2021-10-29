020be3a1846c08cd601838e003af0c9aebb94095
Wojstan <wpstaniszewski@gmail.com>
2021-10-29 15:55:58 +0200

    Add react filters and cookies docs

---
+++ en/_mdb5/react/web/docs/plugins/filters/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Filters',
+  'subheading' => 'React 5 Filters plugin',
+  'seo_title' => 'React Bootstrap Filters - examples & tutorial',
+  'description' => 'Filters plugin built with the latest Bootstrap 5. Supports data array, checkboxes, customized sorting, and much more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/plugins/filters/',
+  'image' => '',
+  'video' => '',
+);
