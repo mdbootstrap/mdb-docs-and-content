b96fbf4f03e7450ee35b78e0a5fc36f0960454e4
ascensus <szymanskimichal9@gmail.com>
2021-11-02 08:13:06 +0100

    Update file input docs

--- en/_mdb5/standard/web/docs/forms/file/data.php
+++ en/_mdb5/standard/web/docs/forms/file/data.php
@@ -1,10 +1,10 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'File',
-  'subheading' => 'Bootstrap 5 File',
-  'seo_title' => 'Bootstrap File - examples & tutorial',
-  'description' => 'File upload built with the latest Bootstrap 5. Helps upload a single or many items. Supports images, files, and documents. Add items directly from your local storage.',
+  'main_title' => 'File upload',
+  'subheading' => 'Bootstrap 5 File upload / File input',
+  'seo_title' => 'Bootstrap File upload / file input - examples & tutorial',
+  'description' => 'File input built with the latest Bootstrap 5. Helps upload a single or many items. Supports images, files, and documents. Add items directly from your local storage.',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/file/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/file/file.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
