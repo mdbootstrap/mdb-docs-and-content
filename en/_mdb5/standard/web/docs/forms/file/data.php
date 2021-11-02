bff74a8cf9f5e45872f85e0f1a42350f4ab88cf1
ascensus <szymanskimichal9@gmail.com>
2021-11-02 08:21:50 +0100

    Update file input docs meta data

--- en/_mdb5/standard/web/docs/forms/file/data.php
+++ en/_mdb5/standard/web/docs/forms/file/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'File upload',
   'subheading' => 'Bootstrap 5 File upload / File input',
   'seo_title' => 'Bootstrap File upload / file input - examples & tutorial',
-  'description' => 'File input built with the latest Bootstrap 5. Helps upload a single or many items. Supports images, files, and documents. Add items directly from your local storage.',
+  'description' => 'File input built with the latest Bootstrap 5. Examples of uploading single or multiple elements. Supports images, files, and documents. Add items directly from your local storage.',
   'url' => 'https://mdbootstrap.com/docs/standard/forms/file/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content-gh/en/_mdb5/standard/web/docs/forms/file/file.jpg',
   'video' => 'https://www.youtube.com/watch?v=c9B4TPnak1A',
