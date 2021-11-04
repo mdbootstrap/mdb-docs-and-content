264484e96b7ecc9667f881970de332402df803d1
Arkadiusz Cacko <a.cacko@mdbootstrap.com>
2021-11-04 09:33:51 +0100

    Add getting started tailwind content

---
+++ en/_mdb5/standard/web/docs/cli/getting-started/tailwind/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Tailwind website - create & deploy the project on MDB GO',
+  'seo_title' => ' How to deploy & host tailwind website - tutorial & free hosting',
+  'description' => 'Create, deploy and host your tailwind website with a single command using MDB GO - open source deployment tool & free hosting',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/getting-started/tailwind/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/cli/about/assets/featured.jpg',
+  'video' => 'https://youtu.be/Bjeewt8K-to',
+);
