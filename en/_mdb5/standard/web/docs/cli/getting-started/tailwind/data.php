370cfe3137cf709d73dbf8587c04895a5723f329
Arkadiusz Cacko <a.cacko@mdbootstrap.com>
2021-11-05 12:15:39 +0100

    Change tailwind page data

--- en/_mdb5/standard/web/docs/cli/getting-started/tailwind/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/tailwind/data.php
@@ -2,9 +2,9 @@
 global $data_var;
 $data_var = array(
   'main_title' => 'Tailwind website - create & deploy the project on MDB GO',
-  'seo_title' => ' How to deploy & host tailwind website - tutorial & free hosting',
-  'description' => 'Create, deploy and host your tailwind website with a single command using MDB GO - open source deployment tool & free hosting',
-  'url' => 'https://mdbootstrap.com/docs/standard/cli/getting-started/tailwind/',
+  'seo_title' => ' Tailwind website - create & deploy',
+  'description' => 'How to deploy & host tailwind website - tutorial & free hosting',
+  'url' => 'https://mdbgo.com/getting-started/tailwind/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/cli/about/assets/featured.jpg',
   'video' => 'https://youtu.be/Bjeewt8K-to',
 );
