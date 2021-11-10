f3236dff4f7685862ca786f227c556e1b3d19a90
Pan Seba <sebos.k10@gmail.com>
2021-11-10 11:53:09 +0100

    Add MDB GO Python docs

---
+++ en/_mdb5/standard/web/docs/cli/getting-started/python/data.php
@@ -0,0 +1,9 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Python - create & deploy the project on MDB GO',
+  'seo_title' => ' How to deploy & host Python app - tutorial & free hosting',
+  'description' => 'Create, deploy and host your Python project with a single command using MDB GO - open source deployment tool & free hosting',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/getting-started/python/',
+  'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/cli/about/assets/featured.jpg',
+);
