6894acaa74948d382808a61cf92d9f3f9362ca24
Marcin Łuczak <m.luczak@mdbootstrap.com>
2021-11-22 12:39:34 +0100

    Add MDB5 Vue vertical rule docs

---
+++ en/_mdb5/vue/web/docs/utilities/vertical-rule/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vertical rule',
+  'subheading' => 'Vue Bootstrap 5 Vertical rule',
+  'seo_title' => 'Vue Vertical rule - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Vertical rule with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/vertical-rule/',
+  'image' => '',
+  'video' => '',
+);
