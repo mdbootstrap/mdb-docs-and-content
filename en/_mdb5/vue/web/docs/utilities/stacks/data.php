e000896c494e024407718f28f9ecec354ba89c6e
Marcin Łuczak <m.luczak@mdbootstrap.com>
2021-11-22 12:30:44 +0100

    Add MDB5 Vue stacks docs

---
+++ en/_mdb5/vue/web/docs/utilities/stacks/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Stacks',
+  'subheading' => 'Vue Bootstrap 5 Stacks',
+  'seo_title' => 'Vue Stacks - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'Vue Stacks with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/utilities/stacks/',
+  'image' => '',
+  'video' => '',
+);
