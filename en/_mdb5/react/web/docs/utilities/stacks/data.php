03a1739425979c0947168bfcacf1cea9ef49d600
Wojstan <wpstaniszewski@gmail.com>
2021-11-26 15:21:00 +0100

    Add react stacks page

---
+++ en/_mdb5/react/web/docs/utilities/stacks/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Stacks',
+  'subheading' => 'React Bootstrap 5 Stacks',
+  'seo_title' => 'React Stacks - examples & tutorial. Bootstrap & Material Design',
+  'description' => 'React Stacks with the latest Bootstrap 5. Multiple code examples, a user-friendly guide, extensive API, and customization tools.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/utilities/stacks/',
+  'image' => '',
+  'video' => '',
+);
