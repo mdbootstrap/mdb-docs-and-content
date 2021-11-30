ca137de610539659afaa1ec4f1ed77588d44698c
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2021-11-30 16:06:32 +0100

    Add JAM Stack tutorial

---
+++ en/_mdb5/standard/web/docs/cli/getting-started/jam/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'JAM Stack - create & deploy the project on MDB GO',
+  'seo_title' => ' How to deploy & host JAM Stack app - tutorial & free hosting',
+  'description' => 'Create, deploy and host your JAM Stack project with a single command using MDB GO - open source deployment tool & free hosting',
+  'url' => 'https://mdbgo.com/getting-started/jam-stack/',
+  'image' => '',
+  'video' => '',
+);
