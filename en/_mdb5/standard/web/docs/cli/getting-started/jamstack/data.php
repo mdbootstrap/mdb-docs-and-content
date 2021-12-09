f53c46bf709808f22744b0c8d2c918481727bab4
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2021-12-09 11:45:51 +0100

    Fix error

--- en/_mdb5/standard/web/docs/cli/getting-started/jamstack/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/jamstack/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Jamstack - create & deploy the project on MDB GO',
   'seo_title' => ' How to deploy & host Jamstack app - tutorial & free hosting',
   'description' => 'Create, deploy and host your Jamstack project with a single command using MDB GO - open source deployment tool & free hosting',
-  'url' => 'https://mdbgo.com/getting-started/jam-stack/',
+  'url' => 'https://mdbgo.com/getting-started/jamstack/',
   'image' => '',
   'video' => '',
 );
