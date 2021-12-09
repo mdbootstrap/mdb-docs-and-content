304c9f0461ceebafa359986938cb447ac5e74cd3
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2021-12-09 15:27:48 +0100

    Add MDB CLI Changelog

---
+++ en/_mdb5/standard/web/docs/cli/getting-started/changelog/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'MDB CLI Changelog',
+  'seo_title' => 'MDB CLI Changelog',
+  'description' => 'This is a changelog of MDB CLI - Open source deployment tool & free hosting',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/getting-started/changelog/',
+  'image' => 'https://mdbootstrap.com/img/Marketing/products/cli/cli-free.jpg',
+  'video' => '',
+);
