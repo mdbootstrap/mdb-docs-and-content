c684c013c23be6d343bfae7501069a9398178c68
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2021-12-09 10:51:03 +0100

    Change 'JAM Stack' to 'Jamstack'

--- en/_mdb5/standard/web/docs/cli/getting-started/jam/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/jam/data.php
@@ -1,9 +1,9 @@
 <?php
 global $data_var;
 $data_var = array(
-  'main_title' => 'JAM Stack - create & deploy the project on MDB GO',
-  'seo_title' => ' How to deploy & host JAM Stack app - tutorial & free hosting',
-  'description' => 'Create, deploy and host your JAM Stack project with a single command using MDB GO - open source deployment tool & free hosting',
+  'main_title' => 'Jamstack - create & deploy the project on MDB GO',
+  'seo_title' => ' How to deploy & host Jamstack app - tutorial & free hosting',
+  'description' => 'Create, deploy and host your Jamstack project with a single command using MDB GO - open source deployment tool & free hosting',
   'url' => 'https://mdbgo.com/getting-started/jam-stack/',
   'image' => '',
   'video' => '',
