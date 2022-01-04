01039885e5eb5d0401133cc6e4fa1696b13bfeae
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2022-01-04 12:52:21 +0100

    add mdb5 react transfer docs

---
+++ en/_mdb5/react/web/docs/plugins/transfer/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Transfer',
+  'subheading' => 'React Bootstrap 5 Transfer plugin',
+  'seo_title' => 'React Bootstrap Transfer - examples & tutorial',
+  'description' => 'Transfer plugin built with the latest Bootstrap 5. Transfer data between two columns. Supports disabled items, one way transfer, pagination, and more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/plugins/transfer/',
+  'image' => '',
+  'video' => '',
+);
