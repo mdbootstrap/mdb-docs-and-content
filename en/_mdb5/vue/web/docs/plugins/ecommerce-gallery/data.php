d51c5c00e9ac34bea017fe21b383c3ae87341bca
Marcin Łuczak <m.luczak@mdbootstrap.com>
2022-01-10 09:34:12 +0100

    Add MDB5 Vue ecommerce gallery docs

---
+++ en/_mdb5/vue/web/docs/plugins/ecommerce-gallery/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue eCommerce gallery',
+  'subheading' => 'Vue Bootstrap 5 eCommerce gallery plugin',
+  'seo_title' => 'Vue Bootstrap eCommerce gallery - examples & tutorial',
+  'description' => 'Responsive eCommerce gallery built with the latest Bootstrap 5 and Vue 3. Customize with zoom effect, carousels, different positions, and much more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/ecommerce-gallery/',
+  'image' => '',
+  'video' => '',
+);
