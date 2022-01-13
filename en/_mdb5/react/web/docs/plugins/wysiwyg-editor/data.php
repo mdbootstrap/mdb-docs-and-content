9265966fba2ff80ee19e6f2d5033b2e02abeaa63
Wojstan <wpstaniszewski@gmail.com>
2022-01-13 15:25:17 +0100

    Add react wysiwyg docs

---
+++ en/_mdb5/react/web/docs/plugins/wysiwyg-editor/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'WYSIWYG editor',
+  'subheading' => 'React Bootstrap 5 WYSIWYG editor plugin',
+  'seo_title' => 'React WYSIWYG editor - examples & tutorial',
+  'description' => 'Responsive WYSIWYG editor built with the latest Bootstrap 5. Plenty of customization examples such as custom colors, translations, and many more.',
+  'url' => 'https://mdbootstrap.com/docs/b5/react/plugins/wysiwyg-editor/',
+  'image' => '',
+  'video' => '',
+);
