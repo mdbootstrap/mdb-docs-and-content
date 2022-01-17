81921b940b42ef06022de4fdb9f3bd12ddb070ff
dwaju <d.wajszczuk@mdbootstrap.com>
2022-01-17 15:46:40 +0100

    add captcha docs

---
+++ en/_mdb5/vue/web/docs/plugins/captcha/data.php
@@ -0,0 +1,11 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'Vue Captcha',
+  'subheading' => 'Vue Bootstrap 5 Captcha plugin',
+  'seo_title' => 'Vue Bootstrap Captcha - examples & tutorial',
+  'description' => 'Captcha plugin built with the latest Bootstrap 5 and Vue 3.',
+  'url' => 'https://mdbootstrap.com/docs/b5/vue/plugins/captcha/',
+  'image' => '',
+  'video' => '',
+);
