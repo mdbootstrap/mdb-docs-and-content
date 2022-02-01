8a73236a00b82d716615d3f784c919a4eb1ace82
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2022-02-01 11:05:43 +0100

    Add MDB GO supported technologies list

---
+++ en/_mdb5/standard/web/docs/cli/supported-technologies/data.php
@@ -0,0 +1,10 @@
+<?php
+global $data_var;
+$data_var = array(
+  'main_title' => 'MDB GO - supported technologies',
+  'seo_title' => 'MDB GO - supported technologies',
+  'description' => 'This is the list of supported technologies by MDB GO - open source deployment tool & free hosting',
+  'url' => 'https://mdbootstrap.com/docs/standard/cli/supported-technologies',
+  'image' => '',
+  'video' => ''
+);
