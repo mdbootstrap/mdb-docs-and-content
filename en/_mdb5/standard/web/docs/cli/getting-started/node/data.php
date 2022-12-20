96e5faf819e5ed16e390943ca033e5cc3847cf76
Katarzyna Pietroń <k.pietron@mdbootstrap.com>
2022-12-20 19:41:19 +0100

    Update node tutorial link

--- en/_mdb5/standard/web/docs/cli/getting-started/node/data.php
+++ en/_mdb5/standard/web/docs/cli/getting-started/node/data.php
@@ -4,7 +4,7 @@ $data_var = array(
   'main_title' => 'Node - create & deploy the project on MDB GO',
   'seo_title' => ' How to deploy & host Node app - tutorial & free hosting',
   'description' => 'Create, deploy and host your Node project with a single command using MDB GO - open source deployment tool & free hosting',
-  'url' => 'https://mdbootstrap.com/docs/standard/cli/getting-started/node/',
+  'url' => 'https://mdbgo.com/docs/tutorials/node/',
   'image' => 'https://mdbootstrap.com/wp-content/themes/mdbootstrap4/content/en/_mdb5/standard/cli/about/assets/featured.webp',
   'video' => 'https://youtu.be/Bjeewt8K-to',
 );
